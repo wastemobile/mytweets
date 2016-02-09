d21a13c97e8108a92a2f83a233a2cbd602ef751f
wastemobile <arik@readlab.com>
2016-02-09 10:19:45 +0800

    2016.02.09-10.19.44

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -2,5 +2,5 @@ var payload_details = {
   "tweets": 14332,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Mon Feb 08 15:56:48 +0000 2016"
+  "updated_at": "Tue Feb 09 02:19:44 +0000 2016"
 }
