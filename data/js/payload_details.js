c21445fd1093137d74367cfd66db2dc1433d6203
wastemobile <arik@readlab.com>
2016-03-02 13:30:39 +0800

    2016.03.02-13.30.39

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 14480,
+  "tweets": 14483,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Tue Mar 01 03:34:24 +0000 2016"
+  "updated_at": "Wed Mar 02 05:30:39 +0000 2016"
 }
--- data/js/tweets/2016_03.js
+++ data/js/tweets/2016_03.js
@@ -1,5 +1,589 @@
 Grailbird.data.tweets_2016_03 = [
   {
+    "created_at": "Tue Mar 01 14:42:44 +0000 2016",
+    "id": 704678261490450436,
+    "id_str": "704678261490450436",
+    "text": "我其實沒關心 Amazon 去年推出的抽獎活動平台 Giveaway，因為服務區域只有美國。但最近 Kindle 電子書也加入可選範圍，讓作家與出版社多了件行銷利器，厲害。 https://t.co/w55EJtDFdi https://t.co/EieFZfOTwa",
+    "truncated": false,
+    "source": "<a href=\"http://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 0,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/w55EJtDFdi",
+          "expanded_url": "http://bit.ly/1RiYekW",
+          "display_url": "bit.ly/1RiYekW",
+          "indices": [
+            87,
+            110
+          ]
+        }
+      ],
+      "media": [
+        {
+          "id": 704678258307039232,
+          "id_str": "704678258307039232",
+          "indices": [
+            111,
+            134
+          ],
+          "media_url": "http://pbs.twimg.com/media/CceFJHNVAAAEmvb.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/CceFJHNVAAAEmvb.jpg",
+          "url": "https://t.co/EieFZfOTwa",
+          "display_url": "pic.twitter.com/EieFZfOTwa",
+          "expanded_url": "http://twitter.com/wastemobile/status/704678261490450436/photo/1",
+          "type": "photo",
+          "sizes": {
+            "small": {
+              "w": 340,
+              "h": 80,
+              "resize": "fit"
+            },
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "medium": {
+              "w": 600,
+              "h": 141,
+              "resize": "fit"
+            },
+            "large": {
+              "w": 1024,
+              "h": 240,
+              "resize": "fit"
+            }
+          }
+        }
+      ]
+    },
+    "extended_entities": {
+      "media": [
+        {
+          "id": 704678258307039232,
+          "id_str": "704678258307039232",
+          "indices": [
+            111,
+            134
+          ],
+          "media_url": "http://pbs.twimg.com/media/CceFJHNVAAAEmvb.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/CceFJHNVAAAEmvb.jpg",
+          "url": "https://t.co/EieFZfOTwa",
+          "display_url": "pic.twitter.com/EieFZfOTwa",
+          "expanded_url": "http://twitter.com/wastemobile/status/704678261490450436/photo/1",
+          "type": "photo",
+          "sizes": {
+            "small": {
+              "w": 340,
+              "h": 80,
+              "resize": "fit"
+            },
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "medium": {
+              "w": 600,
+              "h": 141,
+              "resize": "fit"
+            },
+            "large": {
+              "w": 1024,
+              "h": 240,
+              "resize": "fit"
+            }
+          }
+        }
+      ]
+    },
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Tue Mar 01 11:44:03 +0000 2016",
+    "id": 704633294688690176,
+    "id_str": "704633294688690176",
+    "text": "iA Writer 真是從 3.0 開始轉性，不停的加功能。3.1.2 的 MMD 目錄簡碼與檔案快捷搜尋開啟都很棒（shift + command + O）。 https://t.co/xj9oTUpuI9",
+    "truncated": false,
+    "source": "<a href=\"http://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "quoted_status_id": 704621277818986496,
+    "quoted_status_id_str": "704621277818986496",
+    "quoted_status": {
+      "created_at": "Tue Mar 01 10:56:18 +0000 2016",
+      "id": 704621277818986496,
+      "id_str": "704621277818986496",
+      "text": "\"Open Quickly\" is the fastest way to find and open files from your Library in iA Writer 3.1.2 (Use the shortcut ⇧⌘O) https://t.co/eLqer2hpNl",
+      "truncated": false,
+      "source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 203008363,
+        "id_str": "203008363",
+        "name": "iA Writer",
+        "screen_name": "iAWriter",
+        "location": "On iOS, Mac and Android.",
+        "description": "Over a million people use iA Writer as their quintessential writing machine.",
+        "url": "https://t.co/z6Z2SR8cOg",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "https://t.co/z6Z2SR8cOg",
+                "expanded_url": "http://ia.net/writer",
+                "display_url": "ia.net/writer",
+                "indices": [
+                  0,
+                  23
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 13677,
+        "friends_count": 1,
+        "listed_count": 877,
+        "created_at": "Fri Oct 15 09:47:10 +0000 2010",
+        "favourites_count": 2776,
+        "utc_offset": 32400,
+        "time_zone": "Tokyo",
+        "geo_enabled": false,
+        "verified": false,
+        "statuses_count": 8814,
+        "lang": "en",
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "F0F1F4",
+        "profile_background_image_url": "http://pbs.twimg.com/profile_background_images/608828002/hmloyz0cxz80guoxwyzh.png",
+        "profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/608828002/hmloyz0cxz80guoxwyzh.png",
+        "profile_background_tile": true,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/612990293747625984/hkpbsGz6_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/612990293747625984/hkpbsGz6_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/203008363/1446059304",
+        "profile_link_color": "0099FF",
+        "profile_sidebar_border_color": "FAFAFA",
+        "profile_sidebar_fill_color": "FAFAFA",
+        "profile_text_color": "1A1A1A",
+        "profile_use_background_image": true,
+        "has_extended_profile": false,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": false,
+        "follow_request_sent": false,
+        "notifications": false
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 6,
+      "favorite_count": 39,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+
+        ],
+        "media": [
+          {
+            "id": 704620683431555072,
+            "id_str": "704620683431555072",
+            "indices": [
+              117,
+              140
+            ],
+            "media_url": "http://pbs.twimg.com/ext_tw_video_thumb/704620683431555072/pu/img/JGRuEb26Ep1JsoBq.jpg",
+            "media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/704620683431555072/pu/img/JGRuEb26Ep1JsoBq.jpg",
+            "url": "https://t.co/eLqer2hpNl",
+            "display_url": "pic.twitter.com/eLqer2hpNl",
+            "expanded_url": "http://twitter.com/iAWriter/status/704621277818986496/video/1",
+            "type": "photo",
+            "sizes": {
+              "medium": {
+                "w": 600,
+                "h": 468,
+                "resize": "fit"
+              },
+              "large": {
+                "w": 924,
+                "h": 720,
+                "resize": "fit"
+              },
+              "small": {
+                "w": 340,
+                "h": 265,
+                "resize": "fit"
+              },
+              "thumb": {
+                "w": 150,
+                "h": 150,
+                "resize": "crop"
+              }
+            }
+          }
+        ]
+      },
+      "extended_entities": {
+        "media": [
+          {
+            "id": 704620683431555072,
+            "id_str": "704620683431555072",
+            "indices": [
+              117,
+              140
+            ],
+            "media_url": "http://pbs.twimg.com/ext_tw_video_thumb/704620683431555072/pu/img/JGRuEb26Ep1JsoBq.jpg",
+            "media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/704620683431555072/pu/img/JGRuEb26Ep1JsoBq.jpg",
+            "url": "https://t.co/eLqer2hpNl",
+            "display_url": "pic.twitter.com/eLqer2hpNl",
+            "expanded_url": "http://twitter.com/iAWriter/status/704621277818986496/video/1",
+            "type": "video",
+            "sizes": {
+              "medium": {
+                "w": 600,
+                "h": 468,
+                "resize": "fit"
+              },
+              "large": {
+                "w": 924,
+                "h": 720,
+                "resize": "fit"
+              },
+              "small": {
+                "w": 340,
+                "h": 265,
+                "resize": "fit"
+              },
+              "thumb": {
+                "w": 150,
+                "h": 150,
+                "resize": "crop"
+              }
+            },
+            "video_info": {
+              "aspect_ratio": [
+                77,
+                60
+              ],
+              "duration_millis": 8383,
+              "variants": [
+                {
+                  "bitrate": 832000,
+                  "content_type": "video/mp4",
+                  "url": "https://video.twimg.com/ext_tw_video/704620683431555072/pu/vid/462x360/PzzS1Q6DZ-Nx469c.mp4"
+                },
+                {
+                  "bitrate": 832000,
+                  "content_type": "video/webm",
+                  "url": "https://video.twimg.com/ext_tw_video/704620683431555072/pu/vid/462x360/PzzS1Q6DZ-Nx469c.webm"
+                },
+                {
+                  "bitrate": 320000,
+                  "content_type": "video/mp4",
+                  "url": "https://video.twimg.com/ext_tw_video/704620683431555072/pu/vid/230x180/LmcGtmGsoEJkgJ0K.mp4"
+                },
+                {
+                  "content_type": "application/dash+xml",
+                  "url": "https://video.twimg.com/ext_tw_video/704620683431555072/pu/pl/mVowZaVmpM40mR2d.mpd"
+                },
+                {
+                  "content_type": "application/x-mpegURL",
+                  "url": "https://video.twimg.com/ext_tw_video/704620683431555072/pu/pl/mVowZaVmpM40mR2d.m3u8"
+                },
+                {
+                  "bitrate": 2176000,
+                  "content_type": "video/mp4",
+                  "url": "https://video.twimg.com/ext_tw_video/704620683431555072/pu/vid/924x720/On-4iAueGd_ZXeJK.mp4"
+                }
+              ]
+            }
+          }
+        ]
+      },
+      "favorited": false,
+      "retweeted": false,
+      "possibly_sensitive": false,
+      "lang": "en"
+    },
+    "is_quote_status": true,
+    "retweet_count": 1,
+    "favorite_count": 2,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/xj9oTUpuI9",
+          "expanded_url": "https://twitter.com/iAWriter/status/704621277818986496",
+          "display_url": "twitter.com/iAWriter/statu…",
+          "indices": [
+            81,
+            104
+          ]
+        }
+      ]
+    },
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Tue Mar 01 07:05:25 +0000 2016",
+    "id": 704563172045271040,
+    "id_str": "704563172045271040",
+    "text": "日本角川的作家投稿平台 https://t.co/qgEzdIyrc2 終於上線，首日就爆大話題。曾與角川簽約的輕小說作家ろくごまるに，投稿爆料角川與作家簽獨家合約卻不出版，讓作者走投無路的黑歷史，作家簽約前請詳閱說明書。 https://t.co/01Ooa6KeE2",
+    "truncated": false,
+    "source": "<a href=\"http://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "quoted_status_id": 704560083200450560,
+    "quoted_status_id_str": "704560083200450560",
+    "quoted_status": {
+      "created_at": "Tue Mar 01 06:53:08 +0000 2016",
+      "id": 704560083200450560,
+      "id_str": "704560083200450560",
+      "text": "[はてブ]「KADOKAWAと契約したのに本が出ない」 KADOKAWAの「カクヨム」開設初日に核爆弾級の小説が投稿される - ねとらぼ https://t.co/5Q2fmqn94J  #shuppan",
+      "truncated": false,
+      "source": "<a href=\"http://dlvr.it\" rel=\"nofollow\">dlvr.it</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 9826252,
+        "id_str": "9826252",
+        "name": "booknews",
+        "screen_name": "booknews",
+        "location": "日本",
+        "description": "出版関連のニュースを流します。",
+        "url": "http://t.co/gJz5ajsjoO",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "http://t.co/gJz5ajsjoO",
+                "expanded_url": "http://twitter.g.hatena.ne.jp/worris/20090814",
+                "display_url": "twitter.g.hatena.ne.jp/worris/20090814",
+                "indices": [
+                  0,
+                  22
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 5359,
+        "friends_count": 90,
+        "listed_count": 741,
+        "created_at": "Wed Oct 31 15:14:17 +0000 2007",
+        "favourites_count": 0,
+        "utc_offset": 32400,
+        "time_zone": "Tokyo",
+        "geo_enabled": false,
+        "verified": false,
+        "statuses_count": 23462,
+        "lang": "en",
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "C0DEED",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/707384506/renoir_1890_the_reading_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/707384506/renoir_1890_the_reading_normal.jpg",
+        "profile_link_color": "0084B4",
+        "profile_sidebar_border_color": "C0DEED",
+        "profile_sidebar_fill_color": "DDEEF6",
+        "profile_text_color": "333333",
+        "profile_use_background_image": true,
+        "has_extended_profile": false,
+        "default_profile": true,
+        "default_profile_image": false,
+        "following": true,
+        "follow_request_sent": false,
+        "notifications": false
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 3,
+      "favorite_count": 5,
+      "entities": {
+        "hashtags": [
+          {
+            "text": "shuppan",
+            "indices": [
+              94,
+              102
+            ]
+          }
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/5Q2fmqn94J",
+            "expanded_url": "http://dlvr.it/KfTdm3",
+            "display_url": "dlvr.it/KfTdm3",
+            "indices": [
+              69,
+              92
+            ]
+          }
+        ]
+      },
+      "favorited": false,
+      "retweeted": false,
+      "possibly_sensitive": false,
+      "lang": "ja"
+    },
+    "is_quote_status": true,
+    "retweet_count": 3,
+    "favorite_count": 0,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/qgEzdIyrc2",
+          "expanded_url": "http://bit.ly/1Qp25Rf",
+          "display_url": "bit.ly/1Qp25Rf",
+          "indices": [
+            12,
+            35
+          ]
+        },
+        {
+          "url": "https://t.co/01Ooa6KeE2",
+          "expanded_url": "https://twitter.com/booknews/status/704560083200450560",
+          "display_url": "twitter.com/booknews/statu…",
+          "indices": [
+            112,
+            135
+          ]
+        }
+      ]
+    },
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "ja"
+  },
+  {
     "created_at": "Tue Mar 01 03:14:24 +0000 2016",
     "id": 704505035619590144,
     "id_str": "704505035619590144",
