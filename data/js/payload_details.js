3155d6d5181cbb043981d74b64d5df41e768e2d2
yoyodyne <yoyodyne@Eva.local>
2016-09-25 01:38:23 +0800

    2016.09.25-01.38.13

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -2,5 +2,5 @@ var payload_details = {
   "tweets": 16195,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Sat Sep 24 17:35:59 +0000 2016"
+  "updated_at": "Sat Sep 24 17:37:05 +0000 2016"
 }
