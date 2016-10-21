e8f330e86cafb5edd009f12b295c2f6e0ee5629c
Arik Chao <yoyodyne@Arikde-iMac.local>
2016-10-22 00:48:33 +0800

    2016.10.22-00.48.33

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -2,5 +2,5 @@ var payload_details = {
   "tweets": 16419,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Fri Oct 21 16:48:21 +0000 2016"
+  "updated_at": "Fri Oct 21 16:48:33 +0000 2016"
 }
