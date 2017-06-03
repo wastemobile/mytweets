0a5c6eab1d7f90d3dff1a1ccff867b2306e7e7ea
wastemobile <wastemobile@me.com>
2017-06-03 11:44:41 +0800

    2017.06.03-11.44.41

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -2,5 +2,5 @@ var payload_details = {
   "tweets": 17623,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Sat Jun 03 03:37:02 +0000 2017"
+  "updated_at": "Sat Jun 03 03:44:41 +0000 2017"
 }
