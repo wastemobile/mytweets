00de43cc635492ee6e817c65789b577305446090
wastemobile <wastemobile@me.com>
2018-04-03 21:09:45 +0800

    2018.04.03-21.09.45

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 19167,
+  "tweets": 19170,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Sun Apr 01 14:44:24 +0000 2018"
+  "updated_at": "Tue Apr 03 13:09:45 +0000 2018"
 }
--- data/js/tweets/2018_04.js
+++ data/js/tweets/2018_04.js
@@ -1,5 +1,248 @@
 Grailbird.data.tweets_2018_04 = [
   {
+    "created_at": "Tue Apr 03 03:28:35 +0000 2018",
+    "id": 981010545942831104,
+    "id_str": "981010545942831104",
+    "text": "其實自 2007 開始的這一波數位閱讀與出版，各種創新嘗試不少，陣亡率也極高。\n\nAmazon is Shutting Down Its Crowd-Sourcing Platform, Kindle Scout… https://t.co/tZahvQqqAr",
+    "truncated": true,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/tZahvQqqAr",
+          "expanded_url": "https://twitter.com/i/web/status/981010545942831104",
+          "display_url": "twitter.com/i/web/status/9…",
+          "indices": [
+            108,
+            131
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Mon Apr 02 15:23:06 +0000 2018",
+    "id": 980827975443558400,
+    "id_str": "980827975443558400",
+    "text": "STRAND 雖然在紐約是很有歷史的書店，但應該只有一家門市吧？居然現在要賣自己的 E-Ink 閱讀器！ https://t.co/r7hq8W0ALE https://t.co/gp97EAb4Hu",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/r7hq8W0ALE",
+          "expanded_url": "https://www.strandbooks.com/strindle",
+          "display_url": "strandbooks.com/strindle",
+          "indices": [
+            53,
+            76
+          ]
+        }
+      ],
+      "media": [
+        {
+          "id": 980827956653125635,
+          "id_str": "980827956653125635",
+          "indices": [
+            77,
+            100
+          ],
+          "media_url": "http://pbs.twimg.com/media/DZyZ1HYU8AMtRVe.png",
+          "media_url_https": "https://pbs.twimg.com/media/DZyZ1HYU8AMtRVe.png",
+          "url": "https://t.co/gp97EAb4Hu",
+          "display_url": "pic.twitter.com/gp97EAb4Hu",
+          "expanded_url": "https://twitter.com/wastemobile/status/980827975443558400/photo/1",
+          "type": "photo",
+          "sizes": {
+            "small": {
+              "w": 337,
+              "h": 680,
+              "resize": "fit"
+            },
+            "medium": {
+              "w": 595,
+              "h": 1200,
+              "resize": "fit"
+            },
+            "large": {
+              "w": 600,
+              "h": 1211,
+              "resize": "fit"
+            },
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            }
+          }
+        }
+      ]
+    },
+    "extended_entities": {
+      "media": [
+        {
+          "id": 980827956653125635,
+          "id_str": "980827956653125635",
+          "indices": [
+            77,
+            100
+          ],
+          "media_url": "http://pbs.twimg.com/media/DZyZ1HYU8AMtRVe.png",
+          "media_url_https": "https://pbs.twimg.com/media/DZyZ1HYU8AMtRVe.png",
+          "url": "https://t.co/gp97EAb4Hu",
+          "display_url": "pic.twitter.com/gp97EAb4Hu",
+          "expanded_url": "https://twitter.com/wastemobile/status/980827975443558400/photo/1",
+          "type": "photo",
+          "sizes": {
+            "small": {
+              "w": 337,
+              "h": 680,
+              "resize": "fit"
+            },
+            "medium": {
+              "w": 595,
+              "h": 1200,
+              "resize": "fit"
+            },
+            "large": {
+              "w": 600,
+              "h": 1211,
+              "resize": "fit"
+            },
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            }
+          }
+        }
+      ]
+    },
+    "source": "<a href=\"http://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 1,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Mon Apr 02 12:59:18 +0000 2018",
+    "id": 980791783524261888,
+    "id_str": "980791783524261888",
+    "text": "好聽呢！ https://t.co/IJVsQ98Rfk",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/IJVsQ98Rfk",
+          "expanded_url": "https://itunes.apple.com/tw/album/jedina/983769584",
+          "display_url": "itunes.apple.com/tw/album/jedin…",
+          "indices": [
+            5,
+            28
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "ja"
+  },
+  {
     "created_at": "Sun Apr 01 09:10:46 +0000 2018",
     "id": 980371885924823040,
     "id_str": "980371885924823040",
