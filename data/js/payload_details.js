e9a728c693dbee679c2db6f1670c32b59457e9f8
wastemobile <wastemobile@me.com>
2019-01-19 23:01:24 +0800

    2019.01.19-23.01.23

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -2,5 +2,5 @@ var payload_details = {
   "tweets": 20767,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Sat Jan 19 13:49:24 +0000 2019"
+  "updated_at": "Sat Jan 19 15:01:20 +0000 2019"
 }
