9fb53f656cedec17283f5456247dab44fa3c589d
wastemobile <wastemobile@me.com>
2019-04-02 05:39:52 +0800

    2019.04.02-05.39.52

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 21282,
+  "tweets": 21286,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Mon Apr 01 13:36:21 +0000 2019"
+  "updated_at": "Mon Apr 01 21:39:52 +0000 2019"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,5 +1,343 @@
 Grailbird.data.tweets_2019_04 = [
   {
+    "created_at": "Mon Apr 01 21:29:32 +0000 2019",
+    "id": 1112829371385966592,
+    "id_str": "1112829371385966592",
+    "text": "「近年紙本書閱讀人口減少是事實，工作忙碌無暇讀書是其一，電子書興起取代亦是主因。」\n\n找對政策之前，可能得先找對問題。\n\n願景回響／扶植紙本書 先找對政策 https://t.co/MlZBvt7vA9",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/MlZBvt7vA9",
+          "expanded_url": "http://bit.ly/2I5Xoyj",
+          "display_url": "bit.ly/2I5Xoyj",
+          "indices": [
+            78,
+            101
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Mon Apr 01 15:24:58 +0000 2019",
+    "id": 1112737625880715264,
+    "id_str": "1112737625880715264",
+    "text": "@MrLHu @appleseedTW 其實後來的集數有點刻意與過分用力了，想處理太多重要的議題，但完全可以理解與諒解，難得有這樣的機會嘛！尤其不停看到公視募款廣告時，都想著要那麼多商業電視台幹啥啊？（雖然我已經剪掉有線電視⋯⋯）",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "MrLHu",
+          "name": "Herr Misanthropie, 12+兩棵樹",
+          "id": 496831752,
+          "id_str": "496831752",
+          "indices": [
+            0,
+            6
+          ]
+        },
+        {
+          "screen_name": "appleseedTW",
+          "name": "appleseed",
+          "id": 6102712,
+          "id_str": "6102712",
+          "indices": [
+            7,
+            19
+          ]
+        }
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+    "in_reply_to_status_id": 1112736339202457600,
+    "in_reply_to_status_id_str": "1112736339202457600",
+    "in_reply_to_user_id": 496831752,
+    "in_reply_to_user_id_str": "496831752",
+    "in_reply_to_screen_name": "MrLHu",
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 2,
+    "favorited": false,
+    "retweeted": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Mon Apr 01 15:14:10 +0000 2019",
+    "id": 1112734910719000577,
+    "id_str": "1112734910719000577",
+    "text": "五大國際出版集團中有四個 2018 比 2017 更好，有聲書的成長彌補了電子書的衰退，因此數位部分都有拉升。\n\nA Profitable Year for Trade Publishers… https://t.co/1dPqyUgjSd",
+    "truncated": true,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/1dPqyUgjSd",
+          "expanded_url": "https://twitter.com/i/web/status/1112734910719000577",
+          "display_url": "twitter.com/i/web/status/1…",
+          "indices": [
+            97,
+            120
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 1,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Mon Apr 01 15:02:41 +0000 2019",
+    "id": 1112732020268920832,
+    "id_str": "1112732020268920832",
+    "text": "六、日在公視看了四集，本以為其他地方已經播完，後來才發現是ON檔，但絕對大推！每個人都應該要看！ #我們與惡的距離 https://t.co/wEhsLHzg7X",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+        {
+          "text": "我們與惡的距離",
+          "indices": [
+            49,
+            57
+          ]
+        }
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/wEhsLHzg7X",
+          "expanded_url": "https://twitter.com/appleseedTW/status/1112727323759923202",
+          "display_url": "twitter.com/appleseedTW/st…",
+          "indices": [
+            58,
+            81
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": true,
+    "quoted_status_id": 1112727323759923202,
+    "quoted_status_id_str": "1112727323759923202",
+    "quoted_status": {
+      "created_at": "Mon Apr 01 14:44:02 +0000 2019",
+      "id": 1112727323759923202,
+      "id_str": "1112727323759923202",
+      "text": "剛看了「我們與惡的距離」第一集，非常好看！應該要推薦給更多人知道的啊！",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+
+        ]
+      },
+      "source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 6102712,
+        "id_str": "6102712",
+        "name": "appleseed",
+        "screen_name": "appleseedTW",
+        "location": "Taiwan",
+        "description": "Bio裡該要寫點體面的格言才是。",
+        "url": "https://t.co/jIFRwrlz5U",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "https://t.co/jIFRwrlz5U",
+                "expanded_url": "http://medium.com/@appleseedtw/",
+                "display_url": "medium.com/@appleseedtw/",
+                "indices": [
+                  0,
+                  23
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 2068,
+        "friends_count": 218,
+        "listed_count": 74,
+        "created_at": "Thu May 17 11:02:29 +0000 2007",
+        "favourites_count": 4064,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": false,
+        "verified": false,
+        "statuses_count": 33610,
+        "lang": "zh-tw",
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "C6D4D5",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_tile": true,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/1786421460/Qoo_normal.jpeg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/1786421460/Qoo_normal.jpeg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/6102712/1352462162",
+        "profile_link_color": "0000FF",
+        "profile_sidebar_border_color": "87BC44",
+        "profile_sidebar_fill_color": "E0FF92",
+        "profile_text_color": "000000",
+        "profile_use_background_image": true,
+        "has_extended_profile": false,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": true,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "regular"
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 2,
+      "favorite_count": 10,
+      "favorited": false,
+      "retweeted": false,
+      "lang": "zh"
+    },
+    "retweet_count": 2,
+    "favorite_count": 6,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
     "created_at": "Mon Apr 01 10:56:59 +0000 2019",
     "id": 1112670185582817280,
     "id_str": "1112670185582817280",
