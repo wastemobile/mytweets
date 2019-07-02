452a1d422a4ae1e2b4789aac9a3169f534ce72a2
wastemobile <wastemobile@me.com>
2019-07-03 07:43:13 +0800

    2019.07.03-07.43.12

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 21710,
+  "tweets": 21715,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Tue Jul 02 01:34:46 +0000 2019"
+  "updated_at": "Tue Jul 02 23:43:11 +0000 2019"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,5 +1,657 @@
 Grailbird.data.tweets_2019_07 = [
   {
+    "created_at": "Tue Jul 02 11:55:13 +0000 2019",
+    "id": 1146024526666330113,
+    "id_str": "1146024526666330113",
+    "text": "@appleseedTW 或許是個人喜歡 Vodka 吧，店頭通常分配到一批、賣完就沒得補，只好遇到就掃貨～^_^",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "appleseedTW",
+          "name": "appleseed",
+          "id": 6102712,
+          "id_str": "6102712",
+          "indices": [
+            0,
+            12
+          ]
+        }
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": 1146023488580272129,
+    "in_reply_to_status_id_str": "1146023488580272129",
+    "in_reply_to_user_id": 6102712,
+    "in_reply_to_user_id_str": "6102712",
+    "in_reply_to_screen_name": "appleseedTW",
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 1,
+    "favorited": false,
+    "retweeted": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Tue Jul 02 09:36:26 +0000 2019",
+    "id": 1145989600319459328,
+    "id_str": "1145989600319459328",
+    "text": "一年一度在小 7 看到維京海盜 Vodka 啤酒，同樣採取全店清空策略，只買到 7 罐。 https://t.co/ZMtV3kifHJ",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+
+      ],
+      "media": [
+        {
+          "id": 1145989590928334848,
+          "id_str": "1145989590928334848",
+          "indices": [
+            45,
+            68
+          ],
+          "media_url": "http://pbs.twimg.com/media/D-dfdhOUIAAWaeg.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/D-dfdhOUIAAWaeg.jpg",
+          "url": "https://t.co/ZMtV3kifHJ",
+          "display_url": "pic.twitter.com/ZMtV3kifHJ",
+          "expanded_url": "https://twitter.com/wastemobile/status/1145989600319459328/photo/1",
+          "type": "photo",
+          "sizes": {
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "medium": {
+              "w": 900,
+              "h": 1200,
+              "resize": "fit"
+            },
+            "large": {
+              "w": 1536,
+              "h": 2048,
+              "resize": "fit"
+            },
+            "small": {
+              "w": 510,
+              "h": 680,
+              "resize": "fit"
+            }
+          }
+        }
+      ]
+    },
+    "extended_entities": {
+      "media": [
+        {
+          "id": 1145989590928334848,
+          "id_str": "1145989590928334848",
+          "indices": [
+            45,
+            68
+          ],
+          "media_url": "http://pbs.twimg.com/media/D-dfdhOUIAAWaeg.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/D-dfdhOUIAAWaeg.jpg",
+          "url": "https://t.co/ZMtV3kifHJ",
+          "display_url": "pic.twitter.com/ZMtV3kifHJ",
+          "expanded_url": "https://twitter.com/wastemobile/status/1145989600319459328/photo/1",
+          "type": "photo",
+          "sizes": {
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "medium": {
+              "w": 900,
+              "h": 1200,
+              "resize": "fit"
+            },
+            "large": {
+              "w": 1536,
+              "h": 2048,
+              "resize": "fit"
+            },
+            "small": {
+              "w": 510,
+              "h": 680,
+              "resize": "fit"
+            }
+          }
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 2,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Tue Jul 02 08:29:31 +0000 2019",
+    "id": 1145972760486670337,
+    "id_str": "1145972760486670337",
+    "text": "喔喔，那就來台灣賣紙本書吧～ https://t.co/lEqrIUgFmP",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/lEqrIUgFmP",
+          "expanded_url": "https://twitter.com/jike_collection/status/1145961146785996800",
+          "display_url": "twitter.com/jike_collectio…",
+          "indices": [
+            15,
+            38
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": true,
+    "quoted_status_id": 1145961146785996800,
+    "quoted_status_id_str": "1145961146785996800",
+    "quoted_status": {
+      "created_at": "Tue Jul 02 07:43:22 +0000 2019",
+      "id": 1145961146785996800,
+      "id_str": "1145961146785996800",
+      "text": "亚马逊官方停售纸质书，菜单栏也取消了图书这一分类，但目前仍然可以搜索到第三方卖家提供的纸质书。面临Kindle和阅读App的夹击，纸质书对你来说意味着什么？   #科技圈大小事 https://t.co/KHqrmMRyd7",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+          {
+            "text": "科技圈大小事",
+            "indices": [
+              81,
+              88
+            ]
+          }
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+
+        ],
+        "media": [
+          {
+            "id": 1145961145368379392,
+            "id_str": "1145961145368379392",
+            "indices": [
+              89,
+              112
+            ],
+            "media_url": "http://pbs.twimg.com/media/D-dFlxQVAAAC2tY.jpg",
+            "media_url_https": "https://pbs.twimg.com/media/D-dFlxQVAAAC2tY.jpg",
+            "url": "https://t.co/KHqrmMRyd7",
+            "display_url": "pic.twitter.com/KHqrmMRyd7",
+            "expanded_url": "https://twitter.com/jike_collection/status/1145961146785996800/photo/1",
+            "type": "photo",
+            "sizes": {
+              "medium": {
+                "w": 1200,
+                "h": 410,
+                "resize": "fit"
+              },
+              "thumb": {
+                "w": 150,
+                "h": 150,
+                "resize": "crop"
+              },
+              "small": {
+                "w": 680,
+                "h": 232,
+                "resize": "fit"
+              },
+              "large": {
+                "w": 1334,
+                "h": 456,
+                "resize": "fit"
+              }
+            }
+          }
+        ]
+      },
+      "extended_entities": {
+        "media": [
+          {
+            "id": 1145961145368379392,
+            "id_str": "1145961145368379392",
+            "indices": [
+              89,
+              112
+            ],
+            "media_url": "http://pbs.twimg.com/media/D-dFlxQVAAAC2tY.jpg",
+            "media_url_https": "https://pbs.twimg.com/media/D-dFlxQVAAAC2tY.jpg",
+            "url": "https://t.co/KHqrmMRyd7",
+            "display_url": "pic.twitter.com/KHqrmMRyd7",
+            "expanded_url": "https://twitter.com/jike_collection/status/1145961146785996800/photo/1",
+            "type": "photo",
+            "sizes": {
+              "medium": {
+                "w": 1200,
+                "h": 410,
+                "resize": "fit"
+              },
+              "thumb": {
+                "w": 150,
+                "h": 150,
+                "resize": "crop"
+              },
+              "small": {
+                "w": 680,
+                "h": 232,
+                "resize": "fit"
+              },
+              "large": {
+                "w": 1334,
+                "h": 456,
+                "resize": "fit"
+              }
+            }
+          }
+        ]
+      },
+      "source": "<a href=\"https://15tar.com\" rel=\"nofollow\">即刻精选</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 985402927253635073,
+        "id_str": "985402927253635073",
+        "name": "即刻精选",
+        "screen_name": "jike_collection",
+        "location": "Shenzhen",
+        "description": "精选即刻App有意思的话题更新，非官方。 Telegram：https://t.co/IpyyUse6cd  饭否：https://t.co/Tq2VpQk0uC  Created by @maxhis",
+        "url": null,
+        "entities": {
+          "description": {
+            "urls": [
+              {
+                "url": "https://t.co/IpyyUse6cd",
+                "expanded_url": "http://t.me/jike_collection",
+                "display_url": "t.me/jike_collection",
+                "indices": [
+                  30,
+                  53
+                ]
+              },
+              {
+                "url": "https://t.co/Tq2VpQk0uC",
+                "expanded_url": "http://fanfou.com/jike_bot",
+                "display_url": "fanfou.com/jike_bot",
+                "indices": [
+                  58,
+                  81
+                ]
+              }
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 7093,
+        "friends_count": 1,
+        "listed_count": 44,
+        "created_at": "Sun Apr 15 06:22:20 +0000 2018",
+        "favourites_count": 2,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": false,
+        "verified": false,
+        "statuses_count": 2670,
+        "lang": null,
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "000000",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/985403677350346752/tw6tRCVW_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/985403677350346752/tw6tRCVW_normal.jpg",
+        "profile_link_color": "FAB81E",
+        "profile_sidebar_border_color": "000000",
+        "profile_sidebar_fill_color": "000000",
+        "profile_text_color": "000000",
+        "profile_use_background_image": false,
+        "has_extended_profile": true,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": false,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none"
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 2,
+      "favorite_count": 6,
+      "favorited": false,
+      "retweeted": false,
+      "possibly_sensitive": false,
+      "lang": "zh"
+    },
+    "retweet_count": 1,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Tue Jul 02 07:27:31 +0000 2019",
+    "id": 1145957154832048128,
+    "id_str": "1145957154832048128",
+    "text": "RT @zznn_zz_nn: 費了好些心力，跟廠商溝通、篩選等等我上的衣服終於破百件了，而且完全沒人在乎全館九折滿額免運，明明都是超好看的衣服(;´༎ຶД༎ຶ`)「\nhttps://t.co/kextjUcOXQ",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "zznn_zz_nn",
+          "name": "16",
+          "id": 173201129,
+          "id_str": "173201129",
+          "indices": [
+            3,
+            14
+          ]
+        }
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/kextjUcOXQ",
+          "expanded_url": "https://shopee.tw/shop/8139718/",
+          "display_url": "shopee.tw/shop/8139718/",
+          "indices": [
+            84,
+            107
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Tue Jul 02 07:23:27 +0000 2019",
+      "id": 1145956132646555649,
+      "id_str": "1145956132646555649",
+      "text": "費了好些心力，跟廠商溝通、篩選等等我上的衣服終於破百件了，而且完全沒人在乎全館九折滿額免運，明明都是超好看的衣服(;´༎ຶД༎ຶ`)「\nhttps://t.co/kextjUcOXQ",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/kextjUcOXQ",
+            "expanded_url": "https://shopee.tw/shop/8139718/",
+            "display_url": "shopee.tw/shop/8139718/",
+            "indices": [
+              68,
+              91
+            ]
+          }
+        ]
+      },
+      "source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 173201129,
+        "id_str": "173201129",
+        "name": "16",
+        "screen_name": "zznn_zz_nn",
+        "location": "",
+        "description": "無法停止焦慮的貧困美術生。 我的提問箱https://t.co/LT7IrAboiV",
+        "url": "https://t.co/c1O7bPtT3y",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "https://t.co/c1O7bPtT3y",
+                "expanded_url": "https://p.ecpay.com.tw/7FE14",
+                "display_url": "p.ecpay.com.tw/7FE14",
+                "indices": [
+                  0,
+                  23
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+              {
+                "url": "https://t.co/LT7IrAboiV",
+                "expanded_url": "https://peing.net/zh-TW/holafuasshole",
+                "display_url": "peing.net/zh-TW/holafuas…",
+                "indices": [
+                  19,
+                  42
+                ]
+              }
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 2167,
+        "friends_count": 99,
+        "listed_count": 14,
+        "created_at": "Sat Jul 31 18:28:05 +0000 2010",
+        "favourites_count": 7788,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": false,
+        "verified": false,
+        "statuses_count": 10567,
+        "lang": null,
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "000000",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/1143849307650453504/Dh3VjLFR_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/1143849307650453504/Dh3VjLFR_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/173201129/1554821417",
+        "profile_link_color": "4682B4",
+        "profile_sidebar_border_color": "000000",
+        "profile_sidebar_fill_color": "000000",
+        "profile_text_color": "000000",
+        "profile_use_background_image": false,
+        "has_extended_profile": true,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": true,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none"
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 7,
+      "favorite_count": 26,
+      "favorited": false,
+      "retweeted": true,
+      "possibly_sensitive": true,
+      "lang": "zh"
+    },
+    "is_quote_status": false,
+    "retweet_count": 7,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": true,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Tue Jul 02 07:22:28 +0000 2019",
+    "id": 1145955883651698688,
+    "id_str": "1145955883651698688",
+    "text": "韓總和果凍聽到雷聲應該都躲進室內了吧。",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 1,
+    "favorited": false,
+    "retweeted": false,
+    "lang": "zh"
+  },
+  {
     "created_at": "Mon Jul 01 16:04:08 +0000 2019",
     "id": 1145724780294762498,
     "id_str": "1145724780294762498",
