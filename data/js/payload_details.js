ffd8cf9fb4c9329d0f8cb2e353fcdac05378631e
wastemobile <wastemobile@me.com>
2019-09-04 20:54:59 +0800

    2019.09.04-20.54.58

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 21860,
+  "tweets": 21862,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Mon Sep 02 01:27:39 +0000 2019"
+  "updated_at": "Wed Sep 04 12:54:57 +0000 2019"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,5 +1,596 @@
 Grailbird.data.tweets_2019_09 = [
   {
+    "created_at": "Wed Sep 04 12:22:21 +0000 2019",
+    "id": 1169224175854030849,
+    "id_str": "1169224175854030849",
+    "text": "RT @technews_tw: 輕巧、防水又有實體按鍵！樂天 Kobo 7 吋電子書閱讀器 Kobo Libra H2O 9 月 5 日開放預購 https://t.co/hn4FSYytAK https://t.co/II0YqLGVKM",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "technews_tw",
+          "name": "Technews 科技新報",
+          "id": 2799026833,
+          "id_str": "2799026833",
+          "indices": [
+            3,
+            15
+          ]
+        }
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/hn4FSYytAK",
+          "expanded_url": "http://dlvr.it/RCS5qb",
+          "display_url": "dlvr.it/RCS5qb",
+          "indices": [
+            74,
+            97
+          ]
+        }
+      ],
+      "media": [
+        {
+          "id": 1169220869635203073,
+          "id_str": "1169220869635203073",
+          "indices": [
+            98,
+            121
+          ],
+          "media_url": "http://pbs.twimg.com/media/EDnoL6aUYAEpavq.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/EDnoL6aUYAEpavq.jpg",
+          "url": "https://t.co/II0YqLGVKM",
+          "display_url": "pic.twitter.com/II0YqLGVKM",
+          "expanded_url": "https://twitter.com/technews_tw/status/1169220873246560257/photo/1",
+          "type": "photo",
+          "sizes": {
+            "large": {
+              "w": 1020,
+              "h": 768,
+              "resize": "fit"
+            },
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "small": {
+              "w": 680,
+              "h": 512,
+              "resize": "fit"
+            },
+            "medium": {
+              "w": 1020,
+              "h": 768,
+              "resize": "fit"
+            }
+          },
+          "source_status_id": 1169220873246560257,
+          "source_status_id_str": "1169220873246560257",
+          "source_user_id": 2799026833,
+          "source_user_id_str": "2799026833"
+        }
+      ]
+    },
+    "extended_entities": {
+      "media": [
+        {
+          "id": 1169220869635203073,
+          "id_str": "1169220869635203073",
+          "indices": [
+            98,
+            121
+          ],
+          "media_url": "http://pbs.twimg.com/media/EDnoL6aUYAEpavq.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/EDnoL6aUYAEpavq.jpg",
+          "url": "https://t.co/II0YqLGVKM",
+          "display_url": "pic.twitter.com/II0YqLGVKM",
+          "expanded_url": "https://twitter.com/technews_tw/status/1169220873246560257/photo/1",
+          "type": "photo",
+          "sizes": {
+            "large": {
+              "w": 1020,
+              "h": 768,
+              "resize": "fit"
+            },
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "small": {
+              "w": 680,
+              "h": 512,
+              "resize": "fit"
+            },
+            "medium": {
+              "w": 1020,
+              "h": 768,
+              "resize": "fit"
+            }
+          },
+          "source_status_id": 1169220873246560257,
+          "source_status_id_str": "1169220873246560257",
+          "source_user_id": 2799026833,
+          "source_user_id_str": "2799026833"
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Wed Sep 04 12:09:13 +0000 2019",
+      "id": 1169220873246560257,
+      "id_str": "1169220873246560257",
+      "text": "輕巧、防水又有實體按鍵！樂天 Kobo 7 吋電子書閱讀器 Kobo Libra H2O 9 月 5 日開放預購 https://t.co/hn4FSYytAK https://t.co/II0YqLGVKM",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/hn4FSYytAK",
+            "expanded_url": "http://dlvr.it/RCS5qb",
+            "display_url": "dlvr.it/RCS5qb",
+            "indices": [
+              57,
+              80
+            ]
+          }
+        ],
+        "media": [
+          {
+            "id": 1169220869635203073,
+            "id_str": "1169220869635203073",
+            "indices": [
+              81,
+              104
+            ],
+            "media_url": "http://pbs.twimg.com/media/EDnoL6aUYAEpavq.jpg",
+            "media_url_https": "https://pbs.twimg.com/media/EDnoL6aUYAEpavq.jpg",
+            "url": "https://t.co/II0YqLGVKM",
+            "display_url": "pic.twitter.com/II0YqLGVKM",
+            "expanded_url": "https://twitter.com/technews_tw/status/1169220873246560257/photo/1",
+            "type": "photo",
+            "sizes": {
+              "large": {
+                "w": 1020,
+                "h": 768,
+                "resize": "fit"
+              },
+              "thumb": {
+                "w": 150,
+                "h": 150,
+                "resize": "crop"
+              },
+              "small": {
+                "w": 680,
+                "h": 512,
+                "resize": "fit"
+              },
+              "medium": {
+                "w": 1020,
+                "h": 768,
+                "resize": "fit"
+              }
+            }
+          }
+        ]
+      },
+      "extended_entities": {
+        "media": [
+          {
+            "id": 1169220869635203073,
+            "id_str": "1169220869635203073",
+            "indices": [
+              81,
+              104
+            ],
+            "media_url": "http://pbs.twimg.com/media/EDnoL6aUYAEpavq.jpg",
+            "media_url_https": "https://pbs.twimg.com/media/EDnoL6aUYAEpavq.jpg",
+            "url": "https://t.co/II0YqLGVKM",
+            "display_url": "pic.twitter.com/II0YqLGVKM",
+            "expanded_url": "https://twitter.com/technews_tw/status/1169220873246560257/photo/1",
+            "type": "photo",
+            "sizes": {
+              "large": {
+                "w": 1020,
+                "h": 768,
+                "resize": "fit"
+              },
+              "thumb": {
+                "w": 150,
+                "h": 150,
+                "resize": "crop"
+              },
+              "small": {
+                "w": 680,
+                "h": 512,
+                "resize": "fit"
+              },
+              "medium": {
+                "w": 1020,
+                "h": 768,
+                "resize": "fit"
+              }
+            }
+          }
+        ]
+      },
+      "source": "<a href=\"https://dlvrit.com/\" rel=\"nofollow\">dlvr.it</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 2799026833,
+        "id_str": "2799026833",
+        "name": "Technews 科技新報",
+        "screen_name": "technews_tw",
+        "location": "台灣, 台北市",
+        "description": "科技新報 （TechNews）於 2013 年下半年成立，是一群對資訊科技、能源、半導體、行動運算、網際網路、醫療、生物科技具有高度熱忱與興趣的產業與新媒體人士所共同組成的時代新媒體，以產出有觀點與特色的原創文章為主要任務。",
+        "url": "http://t.co/386e9RC7Ca",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "http://t.co/386e9RC7Ca",
+                "expanded_url": "http://technews.tw",
+                "display_url": "technews.tw",
+                "indices": [
+                  0,
+                  22
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 1204,
+        "friends_count": 30,
+        "listed_count": 40,
+        "created_at": "Tue Sep 09 01:49:20 +0000 2014",
+        "favourites_count": 0,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": false,
+        "verified": false,
+        "statuses_count": 57036,
+        "lang": null,
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "9AE4E8",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/827442298237526016/-VTGpmXn_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/827442298237526016/-VTGpmXn_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/2799026833/1410228878",
+        "profile_link_color": "0084B4",
+        "profile_sidebar_border_color": "BDDCAD",
+        "profile_sidebar_fill_color": "DDFFCC",
+        "profile_text_color": "333333",
+        "profile_use_background_image": true,
+        "has_extended_profile": false,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": true,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none"
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 3,
+      "favorite_count": 3,
+      "favorited": false,
+      "retweeted": true,
+      "possibly_sensitive": false,
+      "lang": "zh"
+    },
+    "is_quote_status": false,
+    "retweet_count": 3,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": true,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Tue Sep 03 09:43:49 +0000 2019",
+    "id": 1168821893480112128,
+    "id_str": "1168821893480112128",
+    "text": "所以高雄真的是無政府狀態⋯⋯ https://t.co/MKyoeh4tyC",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/MKyoeh4tyC",
+          "expanded_url": "https://twitter.com/linlin5400/status/1168743839877648384",
+          "display_url": "twitter.com/linlin5400/sta…",
+          "indices": [
+            15,
+            38
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": true,
+    "quoted_status_id": 1168743839877648384,
+    "quoted_status_id_str": "1168743839877648384",
+    "quoted_status": {
+      "created_at": "Tue Sep 03 04:33:40 +0000 2019",
+      "id": 1168743839877648384,
+      "id_str": "1168743839877648384",
+      "text": "這個拜池說自己是在野的\n在野的高雄市長嗎🤔 https://t.co/O7Mw2nDLC6",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+
+        ],
+        "media": [
+          {
+            "id": 1168743773284651009,
+            "id_str": "1168743773284651009",
+            "indices": [
+              22,
+              45
+            ],
+            "media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1168743773284651009/pu/img/uvTzmIdJs3NJa_jS.jpg",
+            "media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1168743773284651009/pu/img/uvTzmIdJs3NJa_jS.jpg",
+            "url": "https://t.co/O7Mw2nDLC6",
+            "display_url": "pic.twitter.com/O7Mw2nDLC6",
+            "expanded_url": "https://twitter.com/linlin5400/status/1168743839877648384/video/1",
+            "type": "photo",
+            "sizes": {
+              "thumb": {
+                "w": 150,
+                "h": 150,
+                "resize": "crop"
+              },
+              "small": {
+                "w": 383,
+                "h": 680,
+                "resize": "fit"
+              },
+              "large": {
+                "w": 720,
+                "h": 1280,
+                "resize": "fit"
+              },
+              "medium": {
+                "w": 675,
+                "h": 1200,
+                "resize": "fit"
+              }
+            }
+          }
+        ]
+      },
+      "extended_entities": {
+        "media": [
+          {
+            "id": 1168743773284651009,
+            "id_str": "1168743773284651009",
+            "indices": [
+              22,
+              45
+            ],
+            "media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1168743773284651009/pu/img/uvTzmIdJs3NJa_jS.jpg",
+            "media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1168743773284651009/pu/img/uvTzmIdJs3NJa_jS.jpg",
+            "url": "https://t.co/O7Mw2nDLC6",
+            "display_url": "pic.twitter.com/O7Mw2nDLC6",
+            "expanded_url": "https://twitter.com/linlin5400/status/1168743839877648384/video/1",
+            "type": "video",
+            "sizes": {
+              "thumb": {
+                "w": 150,
+                "h": 150,
+                "resize": "crop"
+              },
+              "small": {
+                "w": 383,
+                "h": 680,
+                "resize": "fit"
+              },
+              "large": {
+                "w": 720,
+                "h": 1280,
+                "resize": "fit"
+              },
+              "medium": {
+                "w": 675,
+                "h": 1200,
+                "resize": "fit"
+              }
+            },
+            "video_info": {
+              "aspect_ratio": [
+                9,
+                16
+              ],
+              "duration_millis": 11733,
+              "variants": [
+                {
+                  "bitrate": 832000,
+                  "content_type": "video/mp4",
+                  "url": "https://video.twimg.com/ext_tw_video/1168743773284651009/pu/vid/360x640/E3BOpl86oonP93bE.mp4?tag=10"
+                },
+                {
+                  "bitrate": 2176000,
+                  "content_type": "video/mp4",
+                  "url": "https://video.twimg.com/ext_tw_video/1168743773284651009/pu/vid/720x1280/UG16KCPU1waBXo6t.mp4?tag=10"
+                },
+                {
+                  "content_type": "application/x-mpegURL",
+                  "url": "https://video.twimg.com/ext_tw_video/1168743773284651009/pu/pl/amWZjS-AXQaLlIma.m3u8?tag=10"
+                },
+                {
+                  "bitrate": 632000,
+                  "content_type": "video/mp4",
+                  "url": "https://video.twimg.com/ext_tw_video/1168743773284651009/pu/vid/320x568/HxFCm-hwu2gWTyQd.mp4?tag=10"
+                }
+              ]
+            },
+            "additional_media_info": {
+              "monetizable": false
+            }
+          }
+        ]
+      },
+      "source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 3180714692,
+        "id_str": "3180714692",
+        "name": "̶54拎拎",
+        "screen_name": "linlin5400",
+        "location": "Formosa Bangka",
+        "description": "吾少也賤故能多鄙事\n傲驕的台妹",
+        "url": null,
+        "entities": {
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 5314,
+        "friends_count": 16,
+        "listed_count": 41,
+        "created_at": "Thu Apr 30 14:52:37 +0000 2015",
+        "favourites_count": 13970,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": true,
+        "verified": false,
+        "statuses_count": 18033,
+        "lang": null,
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "C0DEED",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/1160460977197895680/vbtrVYSr_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/1160460977197895680/vbtrVYSr_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/3180714692/1533368754",
+        "profile_link_color": "1DA1F2",
+        "profile_sidebar_border_color": "C0DEED",
+        "profile_sidebar_fill_color": "DDEEF6",
+        "profile_text_color": "333333",
+        "profile_use_background_image": true,
+        "has_extended_profile": false,
+        "default_profile": true,
+        "default_profile_image": false,
+        "following": false,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none"
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 53,
+      "favorite_count": 184,
+      "favorited": false,
+      "retweeted": false,
+      "possibly_sensitive": false,
+      "lang": "zh"
+    },
+    "retweet_count": 0,
+    "favorite_count": 1,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
     "created_at": "Sun Sep 01 01:18:31 +0000 2019",
     "id": 1167969953003139073,
     "id_str": "1167969953003139073",
