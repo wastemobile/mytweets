1f0c2f4c28f9400f335d82b2b52400eb218d934d
wastemobile <wastemobile@me.com>
2019-10-02 12:20:51 +0800

    2019.10.02-12.20.50

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 21953,
+  "tweets": 21954,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Tue Oct 01 05:18:18 +0000 2019"
+  "updated_at": "Wed Oct 02 04:20:50 +0000 2019"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,5 +1,130 @@
 Grailbird.data.tweets_2019_10 = [
   {
+    "created_at": "Tue Oct 01 06:07:10 +0000 2019",
+    "id": 1178914232454152192,
+    "id_str": "1178914232454152192",
+    "text": "iOS 13.1.2⋯⋯ https://t.co/g5PSIIIi6N",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+
+      ],
+      "media": [
+        {
+          "id": 1178914223868370944,
+          "id_str": "1178914223868370944",
+          "indices": [
+            13,
+            36
+          ],
+          "media_url": "http://pbs.twimg.com/media/EFxYPY5UYAAWhPk.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/EFxYPY5UYAAWhPk.jpg",
+          "url": "https://t.co/g5PSIIIi6N",
+          "display_url": "pic.twitter.com/g5PSIIIi6N",
+          "expanded_url": "https://twitter.com/wastemobile/status/1178914232454152192/photo/1",
+          "type": "photo",
+          "sizes": {
+            "medium": {
+              "w": 675,
+              "h": 1200,
+              "resize": "fit"
+            },
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "large": {
+              "w": 750,
+              "h": 1334,
+              "resize": "fit"
+            },
+            "small": {
+              "w": 382,
+              "h": 680,
+              "resize": "fit"
+            }
+          }
+        }
+      ]
+    },
+    "extended_entities": {
+      "media": [
+        {
+          "id": 1178914223868370944,
+          "id_str": "1178914223868370944",
+          "indices": [
+            13,
+            36
+          ],
+          "media_url": "http://pbs.twimg.com/media/EFxYPY5UYAAWhPk.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/EFxYPY5UYAAWhPk.jpg",
+          "url": "https://t.co/g5PSIIIi6N",
+          "display_url": "pic.twitter.com/g5PSIIIi6N",
+          "expanded_url": "https://twitter.com/wastemobile/status/1178914232454152192/photo/1",
+          "type": "photo",
+          "sizes": {
+            "medium": {
+              "w": 675,
+              "h": 1200,
+              "resize": "fit"
+            },
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "large": {
+              "w": 750,
+              "h": 1334,
+              "resize": "fit"
+            },
+            "small": {
+              "w": 382,
+              "h": 680,
+              "resize": "fit"
+            }
+          }
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 1,
+    "favorite_count": 2,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "und"
+  },
+  {
     "created_at": "Tue Oct 01 05:08:52 +0000 2019",
     "id": 1178899559663030272,
     "id_str": "1178899559663030272",
