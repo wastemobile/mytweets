9f119b90a9d5aa40bd2a3d17832b8546cf9d8acd
wastemobile <wastemobile@me.com>
2019-10-06 11:40:14 +0800

    2019.10.06-11.40.12

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 21954,
+  "tweets": 21957,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Fri Oct 04 04:40:10 +0000 2019"
+  "updated_at": "Sun Oct 06 03:40:11 +0000 2019"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,5 +1,266 @@
 Grailbird.data.tweets_2019_10 = [
   {
+    "created_at": "Sat Oct 05 06:42:26 +0000 2019",
+    "id": 1180372658770202627,
+    "id_str": "1180372658770202627",
+    "text": "一堆不用數據的在那說大數據，不用腦的在說 AI，天網反撲真的是剛好而已。",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 4,
+    "favorite_count": 6,
+    "favorited": false,
+    "retweeted": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Sat Oct 05 02:37:32 +0000 2019",
+    "id": 1180311028195708928,
+    "id_str": "1180311028195708928",
+    "text": "在人類很難於平原地區登高騰空俯視的年代，這種圖樣究竟是給誰看的？鳥嗎？\n\n或成地表最大老二：塞那阿巴斯巨人像 https://t.co/O6MuPiStNT https://t.co/OGRSB3JZsr",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/O6MuPiStNT",
+          "expanded_url": "http://bit.ly/2pONuKr",
+          "display_url": "bit.ly/2pONuKr",
+          "indices": [
+            55,
+            78
+          ]
+        }
+      ],
+      "media": [
+        {
+          "id": 1180311014039945216,
+          "id_str": "1180311014039945216",
+          "indices": [
+            79,
+            102
+          ],
+          "media_url": "http://pbs.twimg.com/media/EGFOnRxVAAAx6LQ.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/EGFOnRxVAAAx6LQ.jpg",
+          "url": "https://t.co/OGRSB3JZsr",
+          "display_url": "pic.twitter.com/OGRSB3JZsr",
+          "expanded_url": "https://twitter.com/wastemobile/status/1180311028195708928/photo/1",
+          "type": "photo",
+          "sizes": {
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "small": {
+              "w": 680,
+              "h": 411,
+              "resize": "fit"
+            },
+            "large": {
+              "w": 724,
+              "h": 438,
+              "resize": "fit"
+            },
+            "medium": {
+              "w": 724,
+              "h": 438,
+              "resize": "fit"
+            }
+          }
+        }
+      ]
+    },
+    "extended_entities": {
+      "media": [
+        {
+          "id": 1180311014039945216,
+          "id_str": "1180311014039945216",
+          "indices": [
+            79,
+            102
+          ],
+          "media_url": "http://pbs.twimg.com/media/EGFOnRxVAAAx6LQ.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/EGFOnRxVAAAx6LQ.jpg",
+          "url": "https://t.co/OGRSB3JZsr",
+          "display_url": "pic.twitter.com/OGRSB3JZsr",
+          "expanded_url": "https://twitter.com/wastemobile/status/1180311028195708928/photo/1",
+          "type": "photo",
+          "sizes": {
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "small": {
+              "w": 680,
+              "h": 411,
+              "resize": "fit"
+            },
+            "large": {
+              "w": 724,
+              "h": 438,
+              "resize": "fit"
+            },
+            "medium": {
+              "w": 724,
+              "h": 438,
+              "resize": "fit"
+            }
+          }
+        },
+        {
+          "id": 1180311015159824385,
+          "id_str": "1180311015159824385",
+          "indices": [
+            79,
+            102
+          ],
+          "media_url": "http://pbs.twimg.com/media/EGFOnV8VAAEz6Tq.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/EGFOnV8VAAEz6Tq.jpg",
+          "url": "https://t.co/OGRSB3JZsr",
+          "display_url": "pic.twitter.com/OGRSB3JZsr",
+          "expanded_url": "https://twitter.com/wastemobile/status/1180311028195708928/photo/1",
+          "type": "photo",
+          "sizes": {
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "medium": {
+              "w": 964,
+              "h": 642,
+              "resize": "fit"
+            },
+            "large": {
+              "w": 964,
+              "h": 642,
+              "resize": "fit"
+            },
+            "small": {
+              "w": 680,
+              "h": 453,
+              "resize": "fit"
+            }
+          }
+        }
+      ]
+    },
+    "source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 1,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Fri Oct 04 11:00:36 +0000 2019",
+    "id": 1180075240778432513,
+    "id_str": "1180075240778432513",
+    "text": "嗯，《1Q84》好像正式取代《世界末日與冷酷異境》成為我最喜歡的村上春樹小說了。\n\n可能是因為青豆，應該是。",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 10,
+    "favorited": false,
+    "retweeted": false,
+    "lang": "zh"
+  },
+  {
     "created_at": "Tue Oct 01 06:07:10 +0000 2019",
     "id": 1178914232454152192,
     "id_str": "1178914232454152192",
