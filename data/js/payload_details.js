447fc7df2a2287bc65f9002f2cb0d5f2ee85782d
wastemobile <wastemobile@me.com>
2020-08-02 11:13:37 +0800

    2020.08.02-11.13.37

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 22762,
+  "tweets": 22768,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Sat Aug 01 00:42:23 +0000 2020"
+  "updated_at": "Sun Aug 02 03:13:37 +0000 2020"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,5 +1,587 @@
 Grailbird.data.tweets_2020_08 = [
   {
+    "created_at": "Sat Aug 01 14:44:06 +0000 2020",
+    "id": 1289572619713310721,
+    "id_str": "1289572619713310721",
+    "text": "好先進的感覺⋯⋯\n\n駭客鎖定配置不當的公有雲Docker環境建置殭屍網路，利用區塊鏈技術隱匿其連線行蹤 https://t.co/Gh6B59K9Pz https://t.co/DWN1vpDJW6",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/Gh6B59K9Pz",
+          "expanded_url": "https://bit.ly/39Jx09i",
+          "display_url": "bit.ly/39Jx09i",
+          "indices": [
+            52,
+            75
+          ]
+        }
+      ],
+      "media": [
+        {
+          "id": 1289572612813676544,
+          "id_str": "1289572612813676544",
+          "indices": [
+            76,
+            99
+          ],
+          "media_url": "http://pbs.twimg.com/media/EeV7dXrU4AAWEnR.png",
+          "media_url_https": "https://pbs.twimg.com/media/EeV7dXrU4AAWEnR.png",
+          "url": "https://t.co/DWN1vpDJW6",
+          "display_url": "pic.twitter.com/DWN1vpDJW6",
+          "expanded_url": "https://twitter.com/wastemobile/status/1289572619713310721/photo/1",
+          "type": "photo",
+          "sizes": {
+            "small": {
+              "w": 680,
+              "h": 298,
+              "resize": "fit"
+            },
+            "large": {
+              "w": 960,
+              "h": 420,
+              "resize": "fit"
+            },
+            "medium": {
+              "w": 960,
+              "h": 420,
+              "resize": "fit"
+            },
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            }
+          }
+        }
+      ]
+    },
+    "extended_entities": {
+      "media": [
+        {
+          "id": 1289572612813676544,
+          "id_str": "1289572612813676544",
+          "indices": [
+            76,
+            99
+          ],
+          "media_url": "http://pbs.twimg.com/media/EeV7dXrU4AAWEnR.png",
+          "media_url_https": "https://pbs.twimg.com/media/EeV7dXrU4AAWEnR.png",
+          "url": "https://t.co/DWN1vpDJW6",
+          "display_url": "pic.twitter.com/DWN1vpDJW6",
+          "expanded_url": "https://twitter.com/wastemobile/status/1289572619713310721/photo/1",
+          "type": "photo",
+          "sizes": {
+            "small": {
+              "w": 680,
+              "h": 298,
+              "resize": "fit"
+            },
+            "large": {
+              "w": 960,
+              "h": 420,
+              "resize": "fit"
+            },
+            "medium": {
+              "w": 960,
+              "h": 420,
+              "resize": "fit"
+            },
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            }
+          }
+        }
+      ]
+    },
+    "source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 1,
+    "favorite_count": 5,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Sat Aug 01 13:29:49 +0000 2020",
+    "id": 1289553926090420226,
+    "id_str": "1289553926090420226",
+    "text": "RT @hvcloud: 前几天看到那个翻墙被抓的真是说不出什么感受，作为一个内陆省这辈子都不可能肉身出去的人，对反共、卖国什么的都没有兴趣，我上网只是想看看这世界在发生什么，看看不同人的生活状态，自己想说话时不用那么多自我审查，要求很高吗",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "hvcloud",
+          "name": "白木",
+          "id": 2835757405,
+          "id_str": "2835757405",
+          "indices": [
+            3,
+            11
+          ]
+        }
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Sat Aug 01 00:49:23 +0000 2020",
+      "id": 1289362558915424256,
+      "id_str": "1289362558915424256",
+      "text": "前几天看到那个翻墙被抓的真是说不出什么感受，作为一个内陆省这辈子都不可能肉身出去的人，对反共、卖国什么的都没有兴趣，我上网只是想看看这世界在发生什么，看看不同人的生活状态，自己想说话时不用那么多自我审查，要求很高吗",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+
+        ]
+      },
+      "source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 2835757405,
+        "id_str": "2835757405",
+        "name": "白木",
+        "screen_name": "hvcloud",
+        "location": "中国",
+        "description": "静静的看着",
+        "url": null,
+        "entities": {
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 13,
+        "friends_count": 121,
+        "listed_count": 1,
+        "created_at": "Tue Sep 30 04:46:17 +0000 2014",
+        "favourites_count": 34,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": false,
+        "verified": false,
+        "statuses_count": 937,
+        "lang": null,
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "C0DEED",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/745419969400233984/G4Xs94zf_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/745419969400233984/G4Xs94zf_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/2835757405/1428034832",
+        "profile_link_color": "1DA1F2",
+        "profile_sidebar_border_color": "C0DEED",
+        "profile_sidebar_fill_color": "DDEEF6",
+        "profile_text_color": "333333",
+        "profile_use_background_image": true,
+        "has_extended_profile": false,
+        "default_profile": true,
+        "default_profile_image": false,
+        "following": false,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none"
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 9,
+      "favorite_count": 27,
+      "favorited": false,
+      "retweeted": true,
+      "lang": "zh"
+    },
+    "is_quote_status": false,
+    "retweet_count": 9,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": true,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Sat Aug 01 07:28:03 +0000 2020",
+    "id": 1289462883856703488,
+    "id_str": "1289462883856703488",
+    "text": "@zrkkevin 感謝，我決定假裝有聽懂⋯⋯XD",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "zrkkevin",
+          "name": "kevin",
+          "id": 980322391044759552,
+          "id_str": "980322391044759552",
+          "indices": [
+            0,
+            9
+          ]
+        }
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": 1289462485703995396,
+    "in_reply_to_status_id_str": "1289462485703995396",
+    "in_reply_to_user_id": 980322391044759552,
+    "in_reply_to_user_id_str": "980322391044759552",
+    "in_reply_to_screen_name": "zrkkevin",
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 1,
+    "favorited": false,
+    "retweeted": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Sat Aug 01 07:12:58 +0000 2020",
+    "id": 1289459087462166529,
+    "id_str": "1289459087462166529",
+    "text": "看完神影集《開發者》（Devs）就想找文看看，但完全是「每個字都認識、合起來什麼都看不懂」的狀態⋯⋯\n\n超乎想像的運算力：量子電腦時代來臨，幾件你需要知道的事 https://t.co/o6ia4H4FLi https://t.co/YnBUXUnve8",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/o6ia4H4FLi",
+          "expanded_url": "https://bit.ly/39X8SjL",
+          "display_url": "bit.ly/39X8SjL",
+          "indices": [
+            80,
+            103
+          ]
+        }
+      ],
+      "media": [
+        {
+          "id": 1289459080453472260,
+          "id_str": "1289459080453472260",
+          "indices": [
+            104,
+            127
+          ],
+          "media_url": "http://pbs.twimg.com/media/EeUUM6rUwAQULD3.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/EeUUM6rUwAQULD3.jpg",
+          "url": "https://t.co/YnBUXUnve8",
+          "display_url": "pic.twitter.com/YnBUXUnve8",
+          "expanded_url": "https://twitter.com/wastemobile/status/1289459087462166529/photo/1",
+          "type": "photo",
+          "sizes": {
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "medium": {
+              "w": 810,
+              "h": 569,
+              "resize": "fit"
+            },
+            "large": {
+              "w": 810,
+              "h": 569,
+              "resize": "fit"
+            },
+            "small": {
+              "w": 680,
+              "h": 478,
+              "resize": "fit"
+            }
+          }
+        }
+      ]
+    },
+    "extended_entities": {
+      "media": [
+        {
+          "id": 1289459080453472260,
+          "id_str": "1289459080453472260",
+          "indices": [
+            104,
+            127
+          ],
+          "media_url": "http://pbs.twimg.com/media/EeUUM6rUwAQULD3.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/EeUUM6rUwAQULD3.jpg",
+          "url": "https://t.co/YnBUXUnve8",
+          "display_url": "pic.twitter.com/YnBUXUnve8",
+          "expanded_url": "https://twitter.com/wastemobile/status/1289459087462166529/photo/1",
+          "type": "photo",
+          "sizes": {
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "medium": {
+              "w": 810,
+              "h": 569,
+              "resize": "fit"
+            },
+            "large": {
+              "w": 810,
+              "h": 569,
+              "resize": "fit"
+            },
+            "small": {
+              "w": 680,
+              "h": 478,
+              "resize": "fit"
+            }
+          }
+        }
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 1,
+    "favorite_count": 16,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Sat Aug 01 06:37:20 +0000 2020",
+    "id": 1289450122548375552,
+    "id_str": "1289450122548375552",
+    "text": "@thecat 他們是從1995年開始，每週生產一本摘要。",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "thecat",
+          "name": "Jeremy Lu",
+          "id": 807633,
+          "id_str": "807633",
+          "indices": [
+            0,
+            7
+          ]
+        }
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": 1289449677201354753,
+    "in_reply_to_status_id_str": "1289449677201354753",
+    "in_reply_to_user_id": 807633,
+    "in_reply_to_user_id_str": "807633",
+    "in_reply_to_screen_name": "thecat",
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 1,
+    "favorited": false,
+    "retweeted": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Sat Aug 01 06:33:42 +0000 2020",
+    "id": 1289449209368735744,
+    "id_str": "1289449209368735744",
+    "text": "@thecat 授權來源是 https://t.co/whSB29g1Or，一間紐西蘭的小公司，但經營很久了。",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "thecat",
+          "name": "Jeremy Lu",
+          "id": 807633,
+          "id_str": "807633",
+          "indices": [
+            0,
+            7
+          ]
+        }
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/whSB29g1Or",
+          "expanded_url": "http://Summaries.com",
+          "display_url": "Summaries.com",
+          "indices": [
+            14,
+            37
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": 1289446465706684421,
+    "in_reply_to_status_id_str": "1289446465706684421",
+    "in_reply_to_user_id": 807633,
+    "in_reply_to_user_id_str": "807633",
+    "in_reply_to_screen_name": "thecat",
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 1,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
     "created_at": "Sat Aug 01 00:15:07 +0000 2020",
     "id": 1289353933987573760,
     "id_str": "1289353933987573760",
