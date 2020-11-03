d045be4febe9de43a26fc3ad12fed53d298e00fb
wastemobile <wastemobile@me.com>
2020-11-03 12:16:38 +0800

    2020.11.03-12.16.37

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 23134,
+  "tweets": 23137,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Mon Nov 02 13:16:57 +0000 2020"
+  "updated_at": "Tue Nov 03 04:16:37 +0000 2020"
 }
---
+++ data/js/tweets/2020_11.js
@@ -0,0 +1,448 @@
+Grailbird.data.tweets_2020_11 = [
+  {
+    "created_at": "Tue Nov 03 02:29:08 +0000 2020",
+    "id": 1323452119446253568,
+    "id_str": "1323452119446253568",
+    "text": "RT @TwoCents: @angiesaid 八卦如我，立馬去查。記者都不敢寫出色狼名字。\n\n#福灣莊園 董事長 #許峰嘉 屢次 #性騷擾 工讀生。向 #執行長 #許華仁 (#色狼 之子) 反應，卻只換來「明天不用來了」。\n\n色狼被判刑後，許華仁還發文 #誹謗 被害者，也被…",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+        {
+          "text": "福灣莊園",
+          "indices": [
+            49,
+            54
+          ]
+        },
+        {
+          "text": "許峰嘉",
+          "indices": [
+            59,
+            63
+          ]
+        },
+        {
+          "text": "性騷擾",
+          "indices": [
+            67,
+            71
+          ]
+        },
+        {
+          "text": "執行長",
+          "indices": [
+            78,
+            82
+          ]
+        },
+        {
+          "text": "許華仁",
+          "indices": [
+            83,
+            87
+          ]
+        },
+        {
+          "text": "色狼",
+          "indices": [
+            89,
+            92
+          ]
+        },
+        {
+          "text": "誹謗",
+          "indices": [
+            129,
+            132
+          ]
+        }
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "TwoCents",
+          "name": "My Two Cents",
+          "id": 8374862,
+          "id_str": "8374862",
+          "indices": [
+            3,
+            12
+          ]
+        },
+        {
+          "screen_name": "angiesaid",
+          "name": "清流吉",
+          "id": 242133,
+          "id_str": "242133",
+          "indices": [
+            14,
+            24
+          ]
+        }
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Tue Nov 03 01:21:24 +0000 2020",
+      "id": 1323435074721779713,
+      "id_str": "1323435074721779713",
+      "text": "@angiesaid 八卦如我，立馬去查。記者都不敢寫出色狼名字。\n\n#福灣莊園 董事長 #許峰嘉 屢次 #性騷擾 工讀生。向 #執行長 #許華仁 (#色狼 之子) 反應，卻只換來「明天不用來了」。\n\n色狼被判刑後，許華仁還發文… https://t.co/OHUTbHkHfG",
+      "truncated": true,
+      "entities": {
+        "hashtags": [
+          {
+            "text": "福灣莊園",
+            "indices": [
+              35,
+              40
+            ]
+          },
+          {
+            "text": "許峰嘉",
+            "indices": [
+              45,
+              49
+            ]
+          },
+          {
+            "text": "性騷擾",
+            "indices": [
+              53,
+              57
+            ]
+          },
+          {
+            "text": "執行長",
+            "indices": [
+              64,
+              68
+            ]
+          },
+          {
+            "text": "許華仁",
+            "indices": [
+              69,
+              73
+            ]
+          },
+          {
+            "text": "色狼",
+            "indices": [
+              75,
+              78
+            ]
+          }
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+          {
+            "screen_name": "angiesaid",
+            "name": "清流吉",
+            "id": 242133,
+            "id_str": "242133",
+            "indices": [
+              0,
+              10
+            ]
+          }
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/OHUTbHkHfG",
+            "expanded_url": "https://twitter.com/i/web/status/1323435074721779713",
+            "display_url": "twitter.com/i/web/status/1…",
+            "indices": [
+              116,
+              139
+            ]
+          }
+        ]
+      },
+      "source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+      "in_reply_to_status_id": 1323415591407902722,
+      "in_reply_to_status_id_str": "1323415591407902722",
+      "in_reply_to_user_id": 242133,
+      "in_reply_to_user_id_str": "242133",
+      "in_reply_to_screen_name": "angiesaid",
+      "user": {
+        "id": 8374862,
+        "id_str": "8374862",
+        "name": "My Two Cents",
+        "screen_name": "TwoCents",
+        "location": "Earth",
+        "description": "認真思考是件很累的事。言論自由就是要忍耐不舒服的言論。沒有人是絕對客觀，中立也只是從自己的角度左右看的結果而已。。\n\n我不客觀丶不中立，有我的意識形態，但努力想瞭解自己和別人的腦袋。如果您只想活在和諧的同溫層，敬請加入封鎖的行列，謝謝。。\n\n如果言辭不恭，冒犯到您，也可以說一下。。",
+        "url": null,
+        "entities": {
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 511,
+        "friends_count": 409,
+        "listed_count": 23,
+        "created_at": "Thu Aug 23 04:28:32 +0000 2007",
+        "favourites_count": 9341,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": true,
+        "verified": false,
+        "statuses_count": 28153,
+        "lang": null,
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "000000",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/64431558/fish_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/64431558/fish_normal.jpg",
+        "profile_link_color": "89C9FA",
+        "profile_sidebar_border_color": "000000",
+        "profile_sidebar_fill_color": "000000",
+        "profile_text_color": "000000",
+        "profile_use_background_image": false,
+        "has_extended_profile": false,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": false,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "regular"
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 30,
+      "favorite_count": 25,
+      "favorited": false,
+      "retweeted": true,
+      "possibly_sensitive": false,
+      "lang": "zh"
+    },
+    "is_quote_status": false,
+    "retweet_count": 30,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": true,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Tue Nov 03 01:37:24 +0000 2020",
+    "id": 1323439098900541441,
+    "id_str": "1323439098900541441",
+    "text": "鮑威爾書店推出一款能散發出古舊紙張氣味的中性香水，替疫情期間無法到書店買書的讀者，提供一種沈浸書店氛圍的解決方案。\n\nPowell’s by Powell’s fragrance offers smell of beloved P… https://t.co/WpvOSTKdXO",
+    "truncated": true,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/WpvOSTKdXO",
+          "expanded_url": "https://twitter.com/i/web/status/1323439098900541441",
+          "display_url": "twitter.com/i/web/status/1…",
+          "indices": [
+            117,
+            140
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 5,
+    "favorite_count": 20,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Tue Nov 03 00:55:45 +0000 2020",
+    "id": 1323428618752585728,
+    "id_str": "1323428618752585728",
+    "text": "「要説日本製造了不起的地方，我覺得是各種産品都很容易打開。」\n\n每次遇到很難拆的包裝，都會讓我火冒三丈。\n\n演播室by明子（328）海外日本人的逆文化衝擊：日本製造果然了不起 https://t.co/2Th2WatpvR https://t.co/seV3HTCqlZ",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/2Th2WatpvR",
+          "expanded_url": "https://bit.ly/2TR32c4",
+          "display_url": "bit.ly/2TR32c4",
+          "indices": [
+            88,
+            111
+          ]
+        }
+      ],
+      "media": [
+        {
+          "id": 1323428610787569664,
+          "id_str": "1323428610787569664",
+          "indices": [
+            112,
+            135
+          ],
+          "media_url": "http://pbs.twimg.com/media/El3DT0eU8AARfXt.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/El3DT0eU8AARfXt.jpg",
+          "url": "https://t.co/seV3HTCqlZ",
+          "display_url": "pic.twitter.com/seV3HTCqlZ",
+          "expanded_url": "https://twitter.com/wastemobile/status/1323428618752585728/photo/1",
+          "type": "photo",
+          "sizes": {
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "medium": {
+              "w": 340,
+              "h": 250,
+              "resize": "fit"
+            },
+            "small": {
+              "w": 340,
+              "h": 250,
+              "resize": "fit"
+            },
+            "large": {
+              "w": 340,
+              "h": 250,
+              "resize": "fit"
+            }
+          }
+        }
+      ]
+    },
+    "extended_entities": {
+      "media": [
+        {
+          "id": 1323428610787569664,
+          "id_str": "1323428610787569664",
+          "indices": [
+            112,
+            135
+          ],
+          "media_url": "http://pbs.twimg.com/media/El3DT0eU8AARfXt.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/El3DT0eU8AARfXt.jpg",
+          "url": "https://t.co/seV3HTCqlZ",
+          "display_url": "pic.twitter.com/seV3HTCqlZ",
+          "expanded_url": "https://twitter.com/wastemobile/status/1323428618752585728/photo/1",
+          "type": "photo",
+          "sizes": {
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "medium": {
+              "w": 340,
+              "h": 250,
+              "resize": "fit"
+            },
+            "small": {
+              "w": 340,
+              "h": 250,
+              "resize": "fit"
+            },
+            "large": {
+              "w": 340,
+              "h": 250,
+              "resize": "fit"
+            }
+          }
+        }
+      ]
+    },
+    "source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 2,
+    "favorite_count": 8,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  }
+]
