c02bf04a489b8a14e3eba9a95570b152b1a85a23
wastemobile <wastemobile@me.com>
2020-12-03 11:55:25 +0800

    2020.12.03-11.55.24

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 23231,
+  "tweets": 23233,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Wed Dec 02 02:40:44 +0000 2020"
+  "updated_at": "Thu Dec 03 03:55:24 +0000 2020"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,5 +1,250 @@
 Grailbird.data.tweets_2020_12 = [
   {
+    "created_at": "Thu Dec 03 03:01:53 +0000 2020",
+    "id": 1334331997720702976,
+    "id_str": "1334331997720702976",
+    "text": "RT @tw_reporter_org: 【《報導者》新書上架！邀您閱讀，世界之疫下，全面而深度的紀實】https://t.co/3ZvsJI3GX5\n\nCOVID-19肆虐一年來，這個世界怎麼了？\n當防疫仍是現在進行式，我們跨越國界限制，以文字、影像記錄病毒蔓延後的人間。\n並…",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "tw_reporter_org",
+          "name": "報導者 The Reporter",
+          "id": 810667027308355588,
+          "id_str": "810667027308355588",
+          "indices": [
+            3,
+            19
+          ]
+        }
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/3ZvsJI3GX5",
+          "expanded_url": "https://bit.ly/2HU0iIH",
+          "display_url": "bit.ly/2HU0iIH",
+          "indices": [
+            52,
+            75
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Thu Dec 03 02:20:00 +0000 2020",
+      "id": 1334321455924965378,
+      "id_str": "1334321455924965378",
+      "text": "【《報導者》新書上架！邀您閱讀，世界之疫下，全面而深度的紀實】https://t.co/3ZvsJI3GX5\n\nCOVID-19肆虐一年來，這個世界怎麼了？\n當防疫仍是現在進行式，我們跨越國界限制，以文字、影像記錄病毒蔓延後的人間… https://t.co/iRbzoQcBgv",
+      "truncated": true,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/3ZvsJI3GX5",
+            "expanded_url": "https://bit.ly/2HU0iIH",
+            "display_url": "bit.ly/2HU0iIH",
+            "indices": [
+              31,
+              54
+            ]
+          },
+          {
+            "url": "https://t.co/iRbzoQcBgv",
+            "expanded_url": "https://twitter.com/i/web/status/1334321455924965378",
+            "display_url": "twitter.com/i/web/status/1…",
+            "indices": [
+              117,
+              140
+            ]
+          }
+        ]
+      },
+      "source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 810667027308355588,
+        "id_str": "810667027308355588",
+        "name": "報導者 The Reporter",
+        "screen_name": "tw_reporter_org",
+        "location": "Taipei, Taiwan",
+        "description": "《報導者》是台灣第一個由公益基金會成立的網路媒體，秉持深度、開放、非營利精神，致力於公共領域調查報導。\n若您支持我們的理念，歡迎捐款贊助報導者：https://t.co/MnxRMEKfES",
+        "url": "https://t.co/a54uXjEwrb",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "https://t.co/a54uXjEwrb",
+                "expanded_url": "https://www.twreporter.org/",
+                "display_url": "twreporter.org",
+                "indices": [
+                  0,
+                  23
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+              {
+                "url": "https://t.co/MnxRMEKfES",
+                "expanded_url": "http://bit.ly/350G1Hw",
+                "display_url": "bit.ly/350G1Hw",
+                "indices": [
+                  72,
+                  95
+                ]
+              }
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 10978,
+        "friends_count": 41,
+        "listed_count": 125,
+        "created_at": "Mon Dec 19 02:04:15 +0000 2016",
+        "favourites_count": 57,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": false,
+        "verified": false,
+        "statuses_count": 3247,
+        "lang": null,
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "F5F8FA",
+        "profile_background_image_url": null,
+        "profile_background_image_url_https": null,
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/862871332085653504/Q22zFtWW_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/862871332085653504/Q22zFtWW_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/810667027308355588/1596681143",
+        "profile_link_color": "1DA1F2",
+        "profile_sidebar_border_color": "C0DEED",
+        "profile_sidebar_fill_color": "DDEEF6",
+        "profile_text_color": "333333",
+        "profile_use_background_image": true,
+        "has_extended_profile": false,
+        "default_profile": true,
+        "default_profile_image": false,
+        "following": false,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none"
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 3,
+      "favorite_count": 5,
+      "favorited": false,
+      "retweeted": true,
+      "possibly_sensitive": false,
+      "lang": "zh"
+    },
+    "is_quote_status": false,
+    "retweet_count": 3,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": true,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Thu Dec 03 02:46:16 +0000 2020",
+    "id": 1334328066189926402,
+    "id_str": "1334328066189926402",
+    "text": "瑞典商Storytel繼印度、新加坡、南韓之後，本週啟動亞太區第4站泰國，也是全球第22個區域，提供有聲書與電子書的月費吃到飽服務。\n\n訂閱費約為10塊美金，低於瑞典的$20、高於印度的$4。\n\n2023年前還打算擴張18個地區，… https://t.co/1tpi27Mdth",
+    "truncated": true,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/1tpi27Mdth",
+          "expanded_url": "https://twitter.com/i/web/status/1334328066189926402",
+          "display_url": "twitter.com/i/web/status/1…",
+          "indices": [
+            117,
+            140
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": 1332488950234062849,
+    "in_reply_to_status_id_str": "1332488950234062849",
+    "in_reply_to_user_id": 20709129,
+    "in_reply_to_user_id_str": "20709129",
+    "in_reply_to_screen_name": "wastemobile",
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 2,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
     "created_at": "Tue Dec 01 08:42:42 +0000 2020",
     "id": 1333692992415141888,
     "id_str": "1333692992415141888",
