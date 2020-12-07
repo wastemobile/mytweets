24110f4444879aa45077f89b9ecf583e999c0311
wastemobile <wastemobile@me.com>
2020-12-07 22:31:42 +0800

    2020.12.07-22.31.42

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 23238,
+  "tweets": 23239,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Sun Dec 06 03:38:07 +0000 2020"
+  "updated_at": "Mon Dec 07 14:31:41 +0000 2020"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,5 +1,51 @@
 Grailbird.data.tweets_2020_12 = [
   {
+    "created_at": "Sun Dec 06 14:35:20 +0000 2020",
+    "id": 1335593672046350336,
+    "id_str": "1335593672046350336",
+    "text": "我一直以為持修就只發了一首歌。",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 1,
+    "favorited": false,
+    "retweeted": false,
+    "lang": "zh"
+  },
+  {
     "created_at": "Sun Dec 06 03:08:26 +0000 2020",
     "id": 1335420811251838978,
     "id_str": "1335420811251838978",
