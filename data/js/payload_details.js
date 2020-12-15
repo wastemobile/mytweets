4cc5c80ed4cb5055ae81bb49e1fd355f0ae91e42
wastemobile <wastemobile@me.com>
2020-12-15 22:41:06 +0800

    2020.12.15-22.41.06

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -2,5 +2,5 @@ var payload_details = {
   "tweets": 23265,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Mon Dec 14 03:46:36 +0000 2020"
+  "updated_at": "Tue Dec 15 14:41:06 +0000 2020"
 }
