640a619dd18090cb05b9cac95360ac279453ab69
wastemobile <wastemobile@me.com>
2021-02-04 09:27:30 +0800

    2021.02.04-09.27.30

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 23396,
+  "tweets": 23399,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Tue Feb 02 12:12:27 +0000 2021"
+  "updated_at": "Thu Feb 04 01:27:30 +0000 2021"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,5 +1,245 @@
 Grailbird.data.tweets_2021_02 = [
   {
+    "created_at": "Wed Feb 03 03:21:36 +0000 2021",
+    "id": 1356805004863016960,
+    "id_str": "1356805004863016960",
+    "text": "話說幾小時前SpaceX的星艦SN9測試，起飛順利，中間還進行了一次翻轉，但降落時還是失敗爆炸了⋯⋯\n\n登陸火星果然不容易啊。\n\nhttps://t.co/42NE5ei5bs",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/42NE5ei5bs",
+          "expanded_url": "https://youtu.be/_zZ7fIkpBgs",
+          "display_url": "youtu.be/_zZ7fIkpBgs",
+          "indices": [
+            65,
+            88
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": 1356801741644128257,
+    "in_reply_to_status_id_str": "1356801741644128257",
+    "in_reply_to_user_id": 20709129,
+    "in_reply_to_user_id_str": "20709129",
+    "in_reply_to_screen_name": "wastemobile",
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 2,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Wed Feb 03 03:08:38 +0000 2021",
+    "id": 1356801741644128257,
+    "id_str": "1356801741644128257",
+    "text": "不錯不錯，結果都是地面站硬體的問題，小飛鼠活得不錯，還每天通過台灣上空4次，跑得很快（嗯？\n\n漏夜搶救8天！中央大學終於解碼立方衛星「飛鼠」訊號 https://t.co/Vq2RsKB5Wc https://t.co/kzPdheqICb",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/Vq2RsKB5Wc",
+          "expanded_url": "https://bit.ly/3cC4kST",
+          "display_url": "bit.ly/3cC4kST",
+          "indices": [
+            73,
+            96
+          ]
+        }
+      ],
+      "media": [
+        {
+          "id": 1356801734857826304,
+          "id_str": "1356801734857826304",
+          "indices": [
+            97,
+            120
+          ],
+          "media_url": "http://pbs.twimg.com/media/EtRT_TzVoAAdqVT.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/EtRT_TzVoAAdqVT.jpg",
+          "url": "https://t.co/kzPdheqICb",
+          "display_url": "pic.twitter.com/kzPdheqICb",
+          "expanded_url": "https://twitter.com/wastemobile/status/1356801741644128257/photo/1",
+          "type": "photo",
+          "sizes": {
+            "medium": {
+              "w": 800,
+              "h": 600,
+              "resize": "fit"
+            },
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "small": {
+              "w": 680,
+              "h": 510,
+              "resize": "fit"
+            },
+            "large": {
+              "w": 800,
+              "h": 600,
+              "resize": "fit"
+            }
+          }
+        }
+      ]
+    },
+    "extended_entities": {
+      "media": [
+        {
+          "id": 1356801734857826304,
+          "id_str": "1356801734857826304",
+          "indices": [
+            97,
+            120
+          ],
+          "media_url": "http://pbs.twimg.com/media/EtRT_TzVoAAdqVT.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/EtRT_TzVoAAdqVT.jpg",
+          "url": "https://t.co/kzPdheqICb",
+          "display_url": "pic.twitter.com/kzPdheqICb",
+          "expanded_url": "https://twitter.com/wastemobile/status/1356801741644128257/photo/1",
+          "type": "photo",
+          "sizes": {
+            "medium": {
+              "w": 800,
+              "h": 600,
+              "resize": "fit"
+            },
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "small": {
+              "w": 680,
+              "h": 510,
+              "resize": "fit"
+            },
+            "large": {
+              "w": 800,
+              "h": 600,
+              "resize": "fit"
+            }
+          }
+        }
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": 1354299873974120450,
+    "in_reply_to_status_id_str": "1354299873974120450",
+    "in_reply_to_user_id": 20709129,
+    "in_reply_to_user_id_str": "20709129",
+    "in_reply_to_screen_name": "wastemobile",
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 4,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Tue Feb 02 13:46:24 +0000 2021",
+    "id": 1356599855691436032,
+    "id_str": "1356599855691436032",
+    "text": "天元突破 紅蓮螺巖！ #Netflix",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+        {
+          "text": "Netflix",
+          "indices": [
+            11,
+            19
+          ]
+        }
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": false,
+    "lang": "ja"
+  },
+  {
     "created_at": "Tue Feb 02 02:11:10 +0000 2021",
     "id": 1356424892971847680,
     "id_str": "1356424892971847680",
