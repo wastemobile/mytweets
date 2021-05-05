274525d07a39d6b6fd1ed2a7f7cb963f46878251
wastemobile <wastemobile@me.com>
2021-05-05 10:42:56 +0800

    2021.05.05-10.42.55

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 23667,
+  "tweets": 23671,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Tue May 04 02:42:57 +0000 2021"
+  "updated_at": "Wed May 05 02:42:54 +0000 2021"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,5 +1,610 @@
 Grailbird.data.tweets_2021_05 = [
   {
+    "created_at": "Tue May 04 16:03:07 +0000 2021",
+    "id": 1389611554673336325,
+    "id_str": "1389611554673336325",
+    "text": "真的不知道文策院在幹嘛的，不是貶義、就是單純不知道，只看過一些粗略報導說要花幾個百億，例如投錢去拍電影。\n\n但看到「⋯⋯文策院目前也在設法組建技術團隊，開發AI演算法智慧技術⋯⋯」，直覺可以廢掉別浪費錢了。\n\n台漫平台如何走向國際… https://t.co/8Lgu7ivge0",
+    "truncated": true,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/8Lgu7ivge0",
+          "expanded_url": "https://twitter.com/i/web/status/1389611554673336325",
+          "display_url": "twitter.com/i/web/status/1…",
+          "indices": [
+            117,
+            140
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 4,
+    "favorite_count": 15,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Tue May 04 04:30:07 +0000 2021",
+    "id": 1389437156871995393,
+    "id_str": "1389437156871995393",
+    "text": "真是奇妙，瑞典商Sesamy募集了500萬美金，要提供單本購置型態的電子書與有聲書，主要訴求是無需付高昂的月訂閱費就能擁有數位讀物。\n\nhttps://t.co/Gn2fKOSSMC https://t.co/WpQ1ZLVyx4",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/Gn2fKOSSMC",
+          "expanded_url": "https://bit.ly/3tiWna5",
+          "display_url": "bit.ly/3tiWna5",
+          "indices": [
+            68,
+            91
+          ]
+        }
+      ],
+      "media": [
+        {
+          "id": 1389437151348092929,
+          "id_str": "1389437151348092929",
+          "indices": [
+            92,
+            115
+          ],
+          "media_url": "http://pbs.twimg.com/media/E0hFukPVcAEbDVc.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/E0hFukPVcAEbDVc.jpg",
+          "url": "https://t.co/WpQ1ZLVyx4",
+          "display_url": "pic.twitter.com/WpQ1ZLVyx4",
+          "expanded_url": "https://twitter.com/wastemobile/status/1389437156871995393/photo/1",
+          "type": "photo",
+          "sizes": {
+            "small": {
+              "w": 470,
+              "h": 533,
+              "resize": "fit"
+            },
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "medium": {
+              "w": 470,
+              "h": 533,
+              "resize": "fit"
+            },
+            "large": {
+              "w": 470,
+              "h": 533,
+              "resize": "fit"
+            }
+          }
+        }
+      ]
+    },
+    "extended_entities": {
+      "media": [
+        {
+          "id": 1389437151348092929,
+          "id_str": "1389437151348092929",
+          "indices": [
+            92,
+            115
+          ],
+          "media_url": "http://pbs.twimg.com/media/E0hFukPVcAEbDVc.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/E0hFukPVcAEbDVc.jpg",
+          "url": "https://t.co/WpQ1ZLVyx4",
+          "display_url": "pic.twitter.com/WpQ1ZLVyx4",
+          "expanded_url": "https://twitter.com/wastemobile/status/1389437156871995393/photo/1",
+          "type": "photo",
+          "sizes": {
+            "small": {
+              "w": 470,
+              "h": 533,
+              "resize": "fit"
+            },
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "medium": {
+              "w": 470,
+              "h": 533,
+              "resize": "fit"
+            },
+            "large": {
+              "w": 470,
+              "h": 533,
+              "resize": "fit"
+            }
+          }
+        }
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": 1376357897580384256,
+    "in_reply_to_status_id_str": "1376357897580384256",
+    "in_reply_to_user_id": 20709129,
+    "in_reply_to_user_id_str": "20709129",
+    "in_reply_to_screen_name": "wastemobile",
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 3,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Tue May 04 03:47:12 +0000 2021",
+    "id": 1389426358183366663,
+    "id_str": "1389426358183366663",
+    "text": "看到Intel拿蘋果電腦來打負面廣告，在目前如此懸殊的市佔率差異下，感受到了蘋果晶片真的讓人壓力很大吼～ https://t.co/BXLMcjIx6R",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/BXLMcjIx6R",
+          "expanded_url": "https://twitter.com/intel/status/1389118228874493952",
+          "display_url": "twitter.com/intel/status/1…",
+          "indices": [
+            53,
+            76
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": true,
+    "quoted_status_id": 1389118228874493952,
+    "quoted_status_id_str": "1389118228874493952",
+    "quoted_status": {
+      "created_at": "Mon May 03 07:22:48 +0000 2021",
+      "id": 1389118228874493952,
+      "id_str": "1389118228874493952",
+      "text": "這 Mac 有平板模式嗎？提示：#GoPC",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+          {
+            "text": "GoPC",
+            "indices": [
+              16,
+              21
+            ]
+          }
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+
+        ]
+      },
+      "source": "<a href=\"https://ads-api.twitter.com\" rel=\"nofollow\">Twitter for Advertisers (legacy)</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 2803191,
+        "id_str": "2803191",
+        "name": "Intel",
+        "screen_name": "intel",
+        "location": "Santa Clara, California",
+        "description": "Intel news, views & events about global tech innovation. Looking for support? Tweet @IntelSupport",
+        "url": "https://t.co/5NaT726h32",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "https://t.co/5NaT726h32",
+                "expanded_url": "http://www.intel.com",
+                "display_url": "intel.com",
+                "indices": [
+                  0,
+                  23
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 4876668,
+        "friends_count": 1398,
+        "listed_count": 13838,
+        "created_at": "Thu Mar 29 07:22:48 +0000 2007",
+        "favourites_count": 10344,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": true,
+        "verified": true,
+        "statuses_count": 16181,
+        "lang": null,
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "FFFFFF",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/1301199561713541120/7dPeX1gK_normal.png",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/1301199561713541120/7dPeX1gK_normal.png",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/2803191/1599065126",
+        "profile_link_color": "0070C5",
+        "profile_sidebar_border_color": "FFFFFF",
+        "profile_sidebar_fill_color": "86C6E3",
+        "profile_text_color": "444444",
+        "profile_use_background_image": true,
+        "has_extended_profile": false,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": false,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none",
+        "withheld_in_countries": [
+
+        ]
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 7,
+      "favorite_count": 88,
+      "favorited": false,
+      "retweeted": false,
+      "scopes": {
+        "followers": false
+      },
+      "lang": "ja"
+    },
+    "retweet_count": 0,
+    "favorite_count": 1,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Tue May 04 03:40:17 +0000 2021",
+    "id": 1389424614728601600,
+    "id_str": "1389424614728601600",
+    "text": "RT @ll153cmll: 我住過三間韓國大學宿舍、也在外面租過房子，首爾房價跟台北差不多，可是品質比台灣好上太多。\n都是一個月一萬五左右的房租，韓國光浴室濕分離就加五十分，另外床非上下舖、獨立書桌、衣櫃，租屋處甚至還有廚房。\n不管用什麼去比，同樣的房價台灣的宿舍和租屋狀況真…",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "ll153cmll",
+          "name": "👩🏻‍💻🍒",
+          "id": 1702073942,
+          "id_str": "1702073942",
+          "indices": [
+            3,
+            13
+          ]
+        }
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Mon May 03 12:26:49 +0000 2021",
+      "id": 1389194735625990144,
+      "id_str": "1389194735625990144",
+      "text": "我住過三間韓國大學宿舍、也在外面租過房子，首爾房價跟台北差不多，可是品質比台灣好上太多。\n都是一個月一萬五左右的房租，韓國光浴室濕分離就加五十分，另外床非上下舖、獨立書桌、衣櫃，租屋處甚至還有廚房。\n不管用什麼去比，同樣的房價台灣… https://t.co/5J0EDBQvy4",
+      "truncated": true,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/5J0EDBQvy4",
+            "expanded_url": "https://twitter.com/i/web/status/1389194735625990144",
+            "display_url": "twitter.com/i/web/status/1…",
+            "indices": [
+              117,
+              140
+            ]
+          }
+        ]
+      },
+      "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 1702073942,
+        "id_str": "1702073942",
+        "name": "👩🏻‍💻🍒",
+        "screen_name": "ll153cmll",
+        "location": "Made in Taiwan",
+        "description": "窩哺知道🥺🏳️‍🌈🏳️‍⚧️🌹🌂",
+        "url": null,
+        "entities": {
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 5878,
+        "friends_count": 367,
+        "listed_count": 16,
+        "created_at": "Mon Aug 26 14:25:25 +0000 2013",
+        "favourites_count": 28947,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": false,
+        "verified": false,
+        "statuses_count": 61795,
+        "lang": null,
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "C0DEED",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/1384140455865028616/pfLP-WK1_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/1384140455865028616/pfLP-WK1_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/1702073942/1603723602",
+        "profile_link_color": "F5ABB5",
+        "profile_sidebar_border_color": "000000",
+        "profile_sidebar_fill_color": "DDEEF6",
+        "profile_text_color": "333333",
+        "profile_use_background_image": true,
+        "has_extended_profile": true,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": false,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none",
+        "withheld_in_countries": [
+
+        ]
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": true,
+      "quoted_status_id": 1388916714126073861,
+      "quoted_status_id_str": "1388916714126073861",
+      "quoted_status": {
+        "created_at": "Sun May 02 18:02:04 +0000 2021",
+        "id": 1388916714126073861,
+        "id_str": "1388916714126073861",
+        "text": "我認真ㄉ說，去中國化應該包含宿舍正義\n\ngoogle搜尋大學宿舍真的分不清楚哪些是台灣哪些是中國，全世界到底有什麼國家會把宿舍蓋成這樣",
+        "truncated": false,
+        "entities": {
+          "hashtags": [
+
+          ],
+          "symbols": [
+
+          ],
+          "user_mentions": [
+
+          ],
+          "urls": [
+
+          ]
+        },
+        "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+        "in_reply_to_status_id": null,
+        "in_reply_to_status_id_str": null,
+        "in_reply_to_user_id": null,
+        "in_reply_to_user_id_str": null,
+        "in_reply_to_screen_name": null,
+        "user": {
+          "id": 900209100017078272,
+          "id_str": "900209100017078272",
+          "name": "Faiching➰廢青💉 𝐀𝐂𝐀𝐁",
+          "screen_name": "tropicalvoid",
+          "location": "fountain down the road",
+          "description": "STAY THE FUCK AT HOME / perceived reality／non-existent attention span／inconsistent aesthetics／leftist／tw-indy / kumquat advocate",
+          "url": null,
+          "entities": {
+            "description": {
+              "urls": [
+
+              ]
+            }
+          },
+          "protected": false,
+          "followers_count": 8760,
+          "friends_count": 421,
+          "listed_count": 35,
+          "created_at": "Wed Aug 23 04:12:29 +0000 2017",
+          "favourites_count": 116201,
+          "utc_offset": null,
+          "time_zone": null,
+          "geo_enabled": false,
+          "verified": false,
+          "statuses_count": 53618,
+          "lang": null,
+          "contributors_enabled": false,
+          "is_translator": false,
+          "is_translation_enabled": false,
+          "profile_background_color": "F5F8FA",
+          "profile_background_image_url": null,
+          "profile_background_image_url_https": null,
+          "profile_background_tile": false,
+          "profile_image_url": "http://pbs.twimg.com/profile_images/1362381487056637953/CP0pi4F2_normal.jpg",
+          "profile_image_url_https": "https://pbs.twimg.com/profile_images/1362381487056637953/CP0pi4F2_normal.jpg",
+          "profile_banner_url": "https://pbs.twimg.com/profile_banners/900209100017078272/1520636527",
+          "profile_link_color": "1DA1F2",
+          "profile_sidebar_border_color": "C0DEED",
+          "profile_sidebar_fill_color": "DDEEF6",
+          "profile_text_color": "333333",
+          "profile_use_background_image": true,
+          "has_extended_profile": false,
+          "default_profile": true,
+          "default_profile_image": false,
+          "following": false,
+          "follow_request_sent": false,
+          "notifications": false,
+          "translator_type": "none",
+          "withheld_in_countries": [
+
+          ]
+        },
+        "geo": null,
+        "coordinates": null,
+        "place": null,
+        "contributors": null,
+        "is_quote_status": false,
+        "retweet_count": 28,
+        "favorite_count": 456,
+        "favorited": false,
+        "retweeted": false,
+        "lang": "zh"
+      },
+      "retweet_count": 23,
+      "favorite_count": 179,
+      "favorited": false,
+      "retweeted": true,
+      "possibly_sensitive": false,
+      "lang": "zh"
+    },
+    "is_quote_status": true,
+    "quoted_status_id": 1388916714126073861,
+    "quoted_status_id_str": "1388916714126073861",
+    "retweet_count": 23,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": true,
+    "lang": "zh"
+  },
+  {
     "created_at": "Sat May 01 14:20:16 +0000 2021",
     "id": 1388498509200957447,
     "id_str": "1388498509200957447",
