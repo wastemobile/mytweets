8cf91bee675cb478ec00c7a418ebf92441400190
wastemobile <wastemobile@me.com>
2022-02-07 21:16:41 +0800

    2022.02.07-21.16.41

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -2,5 +2,5 @@ var payload_details = {
   "tweets": 24687,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Mon Feb 07 04:21:24 +0000 2022"
+  "updated_at": "Mon Feb 07 13:16:41 +0000 2022"
 }
