c0145f70c7a641f0af2550085d109f5a717c3350
wastemobile <wastemobile@me.com>
2022-04-14 21:17:12 +0800

    2022.04.14-21.17.12

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 25008,
+  "tweets": 25012,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Wed Apr 13 15:15:40 +0000 2022"
+  "updated_at": "Thu Apr 14 13:17:12 +0000 2022"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,5 +1,483 @@
 Grailbird.data.tweets_2022_04 = [
   {
+    "created_at": "Thu Apr 14 12:39:17 +0000 2022",
+    "id": 1514584074010042368,
+    "id_str": "1514584074010042368",
+    "text": "韓擬解除大部分防疫措施，具體方案明出爐 https://t.co/7jalPZCXP0 https://t.co/kKVUDUOngy",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/7jalPZCXP0",
+          "expanded_url": "https://bit.ly/3EbtWSA",
+          "display_url": "bit.ly/3EbtWSA",
+          "indices": [
+            20,
+            43
+          ]
+        }
+      ],
+      "media": [
+        {
+          "id": 1514584066334490624,
+          "id_str": "1514584066334490624",
+          "indices": [
+            44,
+            67
+          ],
+          "media_url": "http://pbs.twimg.com/media/FQTiLwhVgAAP5ys.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/FQTiLwhVgAAP5ys.jpg",
+          "url": "https://t.co/kKVUDUOngy",
+          "display_url": "pic.twitter.com/kKVUDUOngy",
+          "expanded_url": "https://twitter.com/wastemobile/status/1514584074010042368/photo/1",
+          "type": "photo",
+          "sizes": {
+            "medium": {
+              "w": 1024,
+              "h": 649,
+              "resize": "fit"
+            },
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "large": {
+              "w": 1024,
+              "h": 649,
+              "resize": "fit"
+            },
+            "small": {
+              "w": 680,
+              "h": 431,
+              "resize": "fit"
+            }
+          }
+        }
+      ]
+    },
+    "extended_entities": {
+      "media": [
+        {
+          "id": 1514584066334490624,
+          "id_str": "1514584066334490624",
+          "indices": [
+            44,
+            67
+          ],
+          "media_url": "http://pbs.twimg.com/media/FQTiLwhVgAAP5ys.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/FQTiLwhVgAAP5ys.jpg",
+          "url": "https://t.co/kKVUDUOngy",
+          "display_url": "pic.twitter.com/kKVUDUOngy",
+          "expanded_url": "https://twitter.com/wastemobile/status/1514584074010042368/photo/1",
+          "type": "photo",
+          "sizes": {
+            "medium": {
+              "w": 1024,
+              "h": 649,
+              "resize": "fit"
+            },
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "large": {
+              "w": 1024,
+              "h": 649,
+              "resize": "fit"
+            },
+            "small": {
+              "w": 680,
+              "h": 431,
+              "resize": "fit"
+            }
+          }
+        }
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": 1514145111801618436,
+    "in_reply_to_status_id_str": "1514145111801618436",
+    "in_reply_to_user_id": 20709129,
+    "in_reply_to_user_id_str": "20709129",
+    "in_reply_to_screen_name": "wastemobile",
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 1,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Thu Apr 14 12:22:54 +0000 2022",
+    "id": 1514579952229707777,
+    "id_str": "1514579952229707777",
+    "text": "RT @antfu7: Hey Twitter, I don't usually do this, but I'd like to share with you some personal updates that have happened around us recentl…",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "antfu7",
+          "name": "Anthony Fu",
+          "id": 894590724322213888,
+          "id_str": "894590724322213888",
+          "indices": [
+            3,
+            10
+          ]
+        }
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Thu Apr 14 01:16:03 +0000 2022",
+      "id": 1514412132502159361,
+      "id_str": "1514412132502159361",
+      "text": "Hey Twitter, I don't usually do this, but I'd like to share with you some personal updates that have happened aroun… https://t.co/Q6CNrPJtQO",
+      "truncated": true,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/Q6CNrPJtQO",
+            "expanded_url": "https://twitter.com/i/web/status/1514412132502159361",
+            "display_url": "twitter.com/i/web/status/1…",
+            "indices": [
+              117,
+              140
+            ]
+          }
+        ]
+      },
+      "source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 894590724322213888,
+        "id_str": "894590724322213888",
+        "name": "Anthony Fu",
+        "screen_name": "antfu7",
+        "location": "undefined",
+        "description": "A ship in harbor is safe, but that is not what ships are built for.",
+        "url": "https://t.co/fzb56mm1Dl",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "https://t.co/fzb56mm1Dl",
+                "expanded_url": "https://antfu.me",
+                "display_url": "antfu.me",
+                "indices": [
+                  0,
+                  23
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 17819,
+        "friends_count": 809,
+        "listed_count": 190,
+        "created_at": "Mon Aug 07 16:07:04 +0000 2017",
+        "favourites_count": 7626,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": true,
+        "verified": false,
+        "statuses_count": 1896,
+        "lang": null,
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "000000",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/1255727296532541440/iC5IubYd_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/1255727296532541440/iC5IubYd_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/894590724322213888/1639381054",
+        "profile_link_color": "1CC5E0",
+        "profile_sidebar_border_color": "000000",
+        "profile_sidebar_fill_color": "000000",
+        "profile_text_color": "000000",
+        "profile_use_background_image": false,
+        "has_extended_profile": false,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": false,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none",
+        "withheld_in_countries": [
+
+        ]
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 308,
+      "favorite_count": 1111,
+      "favorited": false,
+      "retweeted": true,
+      "lang": "en"
+    },
+    "is_quote_status": false,
+    "retweet_count": 308,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": true,
+    "lang": "en"
+  },
+  {
+    "created_at": "Thu Apr 14 04:48:45 +0000 2022",
+    "id": 1514465660163690497,
+    "id_str": "1514465660163690497",
+    "text": "把 Mac 上的資料庫（graph）搬進 iCloud 雲碟、取消 git 版本管理，順利同步使用。",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": 1514461364563021827,
+    "in_reply_to_status_id_str": "1514461364563021827",
+    "in_reply_to_user_id": 20709129,
+    "in_reply_to_user_id_str": "20709129",
+    "in_reply_to_screen_name": "wastemobile",
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 1,
+    "favorited": false,
+    "retweeted": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Thu Apr 14 04:31:41 +0000 2022",
+    "id": 1514461364563021827,
+    "id_str": "1514461364563021827",
+    "text": "Logseq iOS App https://t.co/1GvzOeCpqf https://t.co/UzkRElBWje",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/1GvzOeCpqf",
+          "expanded_url": "https://apps.apple.com/us/app/logseq/id1601013908?platform=iphone",
+          "display_url": "apps.apple.com/us/app/logseq/…",
+          "indices": [
+            15,
+            38
+          ]
+        }
+      ],
+      "media": [
+        {
+          "id": 1514461357923467268,
+          "id_str": "1514461357923467268",
+          "indices": [
+            39,
+            62
+          ],
+          "media_url": "http://pbs.twimg.com/media/FQRylMEVgAQwpmq.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/FQRylMEVgAQwpmq.jpg",
+          "url": "https://t.co/UzkRElBWje",
+          "display_url": "pic.twitter.com/UzkRElBWje",
+          "expanded_url": "https://twitter.com/wastemobile/status/1514461364563021827/photo/1",
+          "type": "photo",
+          "sizes": {
+            "medium": {
+              "w": 460,
+              "h": 995,
+              "resize": "fit"
+            },
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "small": {
+              "w": 314,
+              "h": 680,
+              "resize": "fit"
+            },
+            "large": {
+              "w": 460,
+              "h": 995,
+              "resize": "fit"
+            }
+          }
+        }
+      ]
+    },
+    "extended_entities": {
+      "media": [
+        {
+          "id": 1514461357923467268,
+          "id_str": "1514461357923467268",
+          "indices": [
+            39,
+            62
+          ],
+          "media_url": "http://pbs.twimg.com/media/FQRylMEVgAQwpmq.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/FQRylMEVgAQwpmq.jpg",
+          "url": "https://t.co/UzkRElBWje",
+          "display_url": "pic.twitter.com/UzkRElBWje",
+          "expanded_url": "https://twitter.com/wastemobile/status/1514461364563021827/photo/1",
+          "type": "photo",
+          "sizes": {
+            "medium": {
+              "w": 460,
+              "h": 995,
+              "resize": "fit"
+            },
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "small": {
+              "w": 314,
+              "h": 680,
+              "resize": "fit"
+            },
+            "large": {
+              "w": 460,
+              "h": 995,
+              "resize": "fit"
+            }
+          }
+        }
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 3,
+    "favorite_count": 3,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "pt"
+  },
+  {
     "created_at": "Wed Apr 13 07:35:00 +0000 2022",
     "id": 1514145111801618436,
     "id_str": "1514145111801618436",
