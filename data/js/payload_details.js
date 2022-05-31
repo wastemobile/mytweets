c89b0460c2e2eda09c3a3656b8a37f332266d1f0
wastemobile <wastemobile@me.com>
2022-05-31 11:46:20 +0800

    2022.05.31-11.46.20

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 25255,
+  "tweets": 25257,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Tue May 31 02:23:05 +0000 2022"
+  "updated_at": "Tue May 31 03:46:20 +0000 2022"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,5 +1,510 @@
 Grailbird.data.tweets_2022_05 = [
   {
+    "created_at": "Tue May 31 03:26:16 +0000 2022",
+    "id": 1531477133293326336,
+    "id_str": "1531477133293326336",
+    "text": "RT @kris10roos: Just finishing up this book on the many lives of @stewartbrand … for anyone interested in the connections between US 1960s…",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "kris10roos",
+          "name": "kristen roos",
+          "id": 750457063,
+          "id_str": "750457063",
+          "indices": [
+            3,
+            14
+          ]
+        },
+        {
+          "screen_name": "stewartbrand",
+          "name": "Stewart Brand",
+          "id": 4435227552,
+          "id_str": "4435227552",
+          "indices": [
+            65,
+            78
+          ]
+        }
+      ],
+      "urls": [
+
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Tue May 31 02:15:27 +0000 2022",
+      "id": 1531459311611703296,
+      "id_str": "1531459311611703296",
+      "text": "Just finishing up this book on the many lives of @stewartbrand … for anyone interested in the connections between U… https://t.co/VK5cEW4zR8",
+      "truncated": true,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+          {
+            "screen_name": "stewartbrand",
+            "name": "Stewart Brand",
+            "id": 4435227552,
+            "id_str": "4435227552",
+            "indices": [
+              49,
+              62
+            ]
+          }
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/VK5cEW4zR8",
+            "expanded_url": "https://twitter.com/i/web/status/1531459311611703296",
+            "display_url": "twitter.com/i/web/status/1…",
+            "indices": [
+              117,
+              140
+            ]
+          }
+        ]
+      },
+      "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 750457063,
+        "id_str": "750457063",
+        "name": "kristen roos",
+        "screen_name": "kris10roos",
+        "location": "unceded territories/Vancouver",
+        "description": "https://t.co/d8YLUoJs3V (he/him)",
+        "url": "https://t.co/l5n3n5d3Oz",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "https://t.co/l5n3n5d3Oz",
+                "expanded_url": "http://kristenroos.ca",
+                "display_url": "kristenroos.ca",
+                "indices": [
+                  0,
+                  23
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+              {
+                "url": "https://t.co/d8YLUoJs3V",
+                "expanded_url": "http://linktr.ee/kkroos",
+                "display_url": "linktr.ee/kkroos",
+                "indices": [
+                  0,
+                  23
+                ]
+              }
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 5293,
+        "friends_count": 1457,
+        "listed_count": 113,
+        "created_at": "Sat Aug 11 02:08:16 +0000 2012",
+        "favourites_count": 3407,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": false,
+        "verified": false,
+        "statuses_count": 1403,
+        "lang": null,
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "C0DEED",
+        "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/1500380963448057859/-NgMRwOl_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/1500380963448057859/-NgMRwOl_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/750457063/1649639265",
+        "profile_link_color": "D9E6EB",
+        "profile_sidebar_border_color": "C0DEED",
+        "profile_sidebar_fill_color": "DDEEF6",
+        "profile_text_color": "333333",
+        "profile_use_background_image": true,
+        "has_extended_profile": false,
+        "default_profile": false,
+        "default_profile_image": false,
+        "following": false,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none",
+        "withheld_in_countries": [
+
+        ]
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 6,
+      "favorite_count": 38,
+      "favorited": false,
+      "retweeted": true,
+      "possibly_sensitive": false,
+      "lang": "en"
+    },
+    "is_quote_status": false,
+    "retweet_count": 6,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": true,
+    "lang": "en"
+  },
+  {
+    "created_at": "Tue May 31 02:48:35 +0000 2022",
+    "id": 1531467650492006401,
+    "id_str": "1531467650492006401",
+    "text": "RT @iamshin91: 幹這超靠北wwww\n\n*來源自網路 https://t.co/drIaZCnmSA",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "iamshin91",
+          "name": "國民舅公",
+          "id": 1216827742437511168,
+          "id_str": "1216827742437511168",
+          "indices": [
+            3,
+            13
+          ]
+        }
+      ],
+      "urls": [
+
+      ],
+      "media": [
+        {
+          "id": 1531446740347670528,
+          "id_str": "1531446740347670528",
+          "indices": [
+            33,
+            56
+          ],
+          "media_url": "http://pbs.twimg.com/media/FUDKskbaUAAzL_q.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/FUDKskbaUAAzL_q.jpg",
+          "url": "https://t.co/drIaZCnmSA",
+          "display_url": "pic.twitter.com/drIaZCnmSA",
+          "expanded_url": "https://twitter.com/iamshin91/status/1531446745745731584/photo/1",
+          "type": "photo",
+          "sizes": {
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "large": {
+              "w": 1080,
+              "h": 872,
+              "resize": "fit"
+            },
+            "small": {
+              "w": 680,
+              "h": 549,
+              "resize": "fit"
+            },
+            "medium": {
+              "w": 1080,
+              "h": 872,
+              "resize": "fit"
+            }
+          },
+          "source_status_id": 1531446745745731584,
+          "source_status_id_str": "1531446745745731584",
+          "source_user_id": 1216827742437511168,
+          "source_user_id_str": "1216827742437511168"
+        }
+      ]
+    },
+    "extended_entities": {
+      "media": [
+        {
+          "id": 1531446740347670528,
+          "id_str": "1531446740347670528",
+          "indices": [
+            33,
+            56
+          ],
+          "media_url": "http://pbs.twimg.com/media/FUDKskbaUAAzL_q.jpg",
+          "media_url_https": "https://pbs.twimg.com/media/FUDKskbaUAAzL_q.jpg",
+          "url": "https://t.co/drIaZCnmSA",
+          "display_url": "pic.twitter.com/drIaZCnmSA",
+          "expanded_url": "https://twitter.com/iamshin91/status/1531446745745731584/photo/1",
+          "type": "photo",
+          "sizes": {
+            "thumb": {
+              "w": 150,
+              "h": 150,
+              "resize": "crop"
+            },
+            "large": {
+              "w": 1080,
+              "h": 872,
+              "resize": "fit"
+            },
+            "small": {
+              "w": 680,
+              "h": 549,
+              "resize": "fit"
+            },
+            "medium": {
+              "w": 1080,
+              "h": 872,
+              "resize": "fit"
+            }
+          },
+          "source_status_id": 1531446745745731584,
+          "source_status_id_str": "1531446745745731584",
+          "source_user_id": 1216827742437511168,
+          "source_user_id_str": "1216827742437511168"
+        }
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Tue May 31 01:25:31 +0000 2022",
+      "id": 1531446745745731584,
+      "id_str": "1531446745745731584",
+      "text": "幹這超靠北wwww\n\n*來源自網路 https://t.co/drIaZCnmSA",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+
+        ],
+        "media": [
+          {
+            "id": 1531446740347670528,
+            "id_str": "1531446740347670528",
+            "indices": [
+              18,
+              41
+            ],
+            "media_url": "http://pbs.twimg.com/media/FUDKskbaUAAzL_q.jpg",
+            "media_url_https": "https://pbs.twimg.com/media/FUDKskbaUAAzL_q.jpg",
+            "url": "https://t.co/drIaZCnmSA",
+            "display_url": "pic.twitter.com/drIaZCnmSA",
+            "expanded_url": "https://twitter.com/iamshin91/status/1531446745745731584/photo/1",
+            "type": "photo",
+            "sizes": {
+              "thumb": {
+                "w": 150,
+                "h": 150,
+                "resize": "crop"
+              },
+              "large": {
+                "w": 1080,
+                "h": 872,
+                "resize": "fit"
+              },
+              "small": {
+                "w": 680,
+                "h": 549,
+                "resize": "fit"
+              },
+              "medium": {
+                "w": 1080,
+                "h": 872,
+                "resize": "fit"
+              }
+            }
+          }
+        ]
+      },
+      "extended_entities": {
+        "media": [
+          {
+            "id": 1531446740347670528,
+            "id_str": "1531446740347670528",
+            "indices": [
+              18,
+              41
+            ],
+            "media_url": "http://pbs.twimg.com/media/FUDKskbaUAAzL_q.jpg",
+            "media_url_https": "https://pbs.twimg.com/media/FUDKskbaUAAzL_q.jpg",
+            "url": "https://t.co/drIaZCnmSA",
+            "display_url": "pic.twitter.com/drIaZCnmSA",
+            "expanded_url": "https://twitter.com/iamshin91/status/1531446745745731584/photo/1",
+            "type": "photo",
+            "sizes": {
+              "thumb": {
+                "w": 150,
+                "h": 150,
+                "resize": "crop"
+              },
+              "large": {
+                "w": 1080,
+                "h": 872,
+                "resize": "fit"
+              },
+              "small": {
+                "w": 680,
+                "h": 549,
+                "resize": "fit"
+              },
+              "medium": {
+                "w": 1080,
+                "h": 872,
+                "resize": "fit"
+              }
+            }
+          }
+        ]
+      },
+      "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 1216827742437511168,
+        "id_str": "1216827742437511168",
+        "name": "國民舅公",
+        "screen_name": "iamshin91",
+        "location": "鈴蘭男子高中",
+        "description": "#超商日常 每天都是沒得彩排的實境秀",
+        "url": null,
+        "entities": {
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 7106,
+        "friends_count": 983,
+        "listed_count": 26,
+        "created_at": "Mon Jan 13 21:02:04 +0000 2020",
+        "favourites_count": 122763,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": true,
+        "verified": false,
+        "statuses_count": 47720,
+        "lang": null,
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "F5F8FA",
+        "profile_background_image_url": null,
+        "profile_background_image_url_https": null,
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/1530714605416480769/UMuJyIbr_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/1530714605416480769/UMuJyIbr_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/1216827742437511168/1588980151",
+        "profile_link_color": "1DA1F2",
+        "profile_sidebar_border_color": "C0DEED",
+        "profile_sidebar_fill_color": "DDEEF6",
+        "profile_text_color": "333333",
+        "profile_use_background_image": true,
+        "has_extended_profile": true,
+        "default_profile": true,
+        "default_profile_image": false,
+        "following": false,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none",
+        "withheld_in_countries": [
+
+        ]
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 32,
+      "favorite_count": 244,
+      "favorited": false,
+      "retweeted": true,
+      "possibly_sensitive": true,
+      "lang": "zh"
+    },
+    "is_quote_status": false,
+    "retweet_count": 32,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": true,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
     "created_at": "Tue May 31 02:13:13 +0000 2022",
     "id": 1531458749126148096,
     "id_str": "1531458749126148096",
