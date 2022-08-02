94921a3b9b44322ce308e9e47bb5ab87e5349ab5
wastemobile <wastemobile@me.com>
2022-08-02 11:13:36 +0800

    2022.08.02-11.13.36

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,6 +1,6 @@
 var payload_details = {
-  "tweets": 25481,
+  "tweets": 25485,
   "created_at": "2015-09-12 02:24:39 +0000",
   "lang": "zh-tw",
-  "updated_at": "Mon Aug 01 03:53:48 +0000 2022"
+  "updated_at": "Tue Aug 02 03:13:36 +0000 2022"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,5 +1,352 @@
 Grailbird.data.tweets_2022_08 = [
   {
+    "created_at": "Tue Aug 02 02:08:28 +0000 2022",
+    "id": 1554287991018754048,
+    "id_str": "1554287991018754048",
+    "text": "Bookstat 被號稱成長最快的數位出版商 Podium Audio 併購了。\n\n成立5年的 Bookstat 以追蹤所有線上書籍的銷售著稱，相較傳統 Nielsen PubTrack 與美國出版商協會（AAP）的 Statsh… https://t.co/Qo8qq9WVXh",
+    "truncated": true,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/Qo8qq9WVXh",
+          "expanded_url": "https://twitter.com/i/web/status/1554287991018754048",
+          "display_url": "twitter.com/i/web/status/1…",
+          "indices": [
+            117,
+            140
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": 1243710575558844416,
+    "in_reply_to_status_id_str": "1243710575558844416",
+    "in_reply_to_user_id": 20709129,
+    "in_reply_to_user_id_str": "20709129",
+    "in_reply_to_screen_name": "wastemobile",
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Tue Aug 02 00:55:42 +0000 2022",
+    "id": 1554269678104563712,
+    "id_str": "1554269678104563712",
+    "text": "RT @futurenote2040: 宛如科幻小說的雛型城市\n\n對於世界創新來說，深圳的地位不亞於矽谷。更正確的來說，矽谷如果沒有深圳的快速製造系統，許多產品只能存在於紙上談兵階段，或者是非常昂貴的有錢人玩具。\n\nhttps://t.co/hnijMLLxdl",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+        {
+          "screen_name": "futurenote2040",
+          "name": "未來手帖 🇺🇦",
+          "id": 1237747327672057856,
+          "id_str": "1237747327672057856",
+          "indices": [
+            3,
+            18
+          ]
+        }
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/hnijMLLxdl",
+          "expanded_url": "https://tungjen.designband.org/%e5%ae%9b%e5%a6%82%e7%a7%91%e5%b9%bb%e5%b0%8f%e8%aa%aa%e7%9a%84%e9%9b%9b%e5%9e%8b%e5%9f%8e%e5%b8%82/",
+          "display_url": "tungjen.designband.org/%e5%ae%9b%e5%a…",
+          "indices": [
+            108,
+            131
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "retweeted_status": {
+      "created_at": "Tue Aug 02 00:27:58 +0000 2022",
+      "id": 1554262697428271104,
+      "id_str": "1554262697428271104",
+      "text": "宛如科幻小說的雛型城市\n\n對於世界創新來說，深圳的地位不亞於矽谷。更正確的來說，矽谷如果沒有深圳的快速製造系統，許多產品只能存在於紙上談兵階段，或者是非常昂貴的有錢人玩具。\n\nhttps://t.co/hnijMLLxdl",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+
+        ],
+        "symbols": [
+
+        ],
+        "user_mentions": [
+
+        ],
+        "urls": [
+          {
+            "url": "https://t.co/hnijMLLxdl",
+            "expanded_url": "https://tungjen.designband.org/%e5%ae%9b%e5%a6%82%e7%a7%91%e5%b9%bb%e5%b0%8f%e8%aa%aa%e7%9a%84%e9%9b%9b%e5%9e%8b%e5%9f%8e%e5%b8%82/",
+            "display_url": "tungjen.designband.org/%e5%ae%9b%e5%a…",
+            "indices": [
+              88,
+              111
+            ]
+          }
+        ]
+      },
+      "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "user": {
+        "id": 1237747327672057856,
+        "id_str": "1237747327672057856",
+        "name": "未來手帖 🇺🇦",
+        "screen_name": "futurenote2040",
+        "location": "Taipei",
+        "description": "未來或許永遠不會來，或許就在當下。",
+        "url": "https://t.co/r7DSW1tHaU",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "url": "https://t.co/r7DSW1tHaU",
+                "expanded_url": "http://tungjen.designband.org",
+                "display_url": "tungjen.designband.org",
+                "indices": [
+                  0,
+                  23
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+
+            ]
+          }
+        },
+        "protected": false,
+        "followers_count": 570,
+        "friends_count": 315,
+        "listed_count": 14,
+        "created_at": "Wed Mar 11 14:29:06 +0000 2020",
+        "favourites_count": 805,
+        "utc_offset": null,
+        "time_zone": null,
+        "geo_enabled": false,
+        "verified": false,
+        "statuses_count": 1816,
+        "lang": null,
+        "contributors_enabled": false,
+        "is_translator": false,
+        "is_translation_enabled": false,
+        "profile_background_color": "F5F8FA",
+        "profile_background_image_url": null,
+        "profile_background_image_url_https": null,
+        "profile_background_tile": false,
+        "profile_image_url": "http://pbs.twimg.com/profile_images/1488819643834073088/s2QSnmnq_normal.jpg",
+        "profile_image_url_https": "https://pbs.twimg.com/profile_images/1488819643834073088/s2QSnmnq_normal.jpg",
+        "profile_banner_url": "https://pbs.twimg.com/profile_banners/1237747327672057856/1585708519",
+        "profile_link_color": "1DA1F2",
+        "profile_sidebar_border_color": "C0DEED",
+        "profile_sidebar_fill_color": "DDEEF6",
+        "profile_text_color": "333333",
+        "profile_use_background_image": true,
+        "has_extended_profile": false,
+        "default_profile": true,
+        "default_profile_image": false,
+        "following": true,
+        "follow_request_sent": false,
+        "notifications": false,
+        "translator_type": "none",
+        "withheld_in_countries": [
+
+        ]
+      },
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": false,
+      "retweet_count": 2,
+      "favorite_count": 1,
+      "favorited": false,
+      "retweeted": true,
+      "possibly_sensitive": false,
+      "lang": "zh"
+    },
+    "is_quote_status": false,
+    "retweet_count": 2,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": true,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Tue Aug 02 00:51:39 +0000 2022",
+    "id": 1554268655512276994,
+    "id_str": "1554268655512276994",
+    "text": "15 至 49 歲的韓國人口中有一半（50.3%）在 7 天內看過 Webtoon，20 世代的使用者 65.6% 佔比最高，連 40 世代的使用者都佔了 32.6%。\n\n手機上的縱向 K-Webtoon 確實是韓國新創。\n\n韓國… https://t.co/kH6wlWNqmT",
+    "truncated": true,
+    "entities": {
+      "hashtags": [
+
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/kH6wlWNqmT",
+          "expanded_url": "https://twitter.com/i/web/status/1554268655512276994",
+          "display_url": "twitter.com/i/web/status/1…",
+          "indices": [
+            117,
+            140
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 5,
+    "favorite_count": 17,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
+    "created_at": "Mon Aug 01 13:22:17 +0000 2022",
+    "id": 1554095171112800258,
+    "id_str": "1554095171112800258",
+    "text": "【輕小說排行】實教, OVERLORD, 史來姆, 誰是新世代的霸主? 帶你看2022年「銷量最高」的50部輕小說!｜井川一 #note https://t.co/ORP3ngg6RT",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+        {
+          "text": "note",
+          "indices": [
+            63,
+            68
+          ]
+        }
+      ],
+      "symbols": [
+
+      ],
+      "user_mentions": [
+
+      ],
+      "urls": [
+        {
+          "url": "https://t.co/ORP3ngg6RT",
+          "expanded_url": "https://youtu.be/br0_u4S8uKo",
+          "display_url": "youtu.be/br0_u4S8uKo",
+          "indices": [
+            69,
+            92
+          ]
+        }
+      ]
+    },
+    "source": "<a href=\"http://itunes.apple.com/us/app/twitter/id409789998?mt=12\" rel=\"nofollow\">Twitter for Mac</a>",
+    "in_reply_to_status_id": null,
+    "in_reply_to_status_id_str": null,
+    "in_reply_to_user_id": null,
+    "in_reply_to_user_id_str": null,
+    "in_reply_to_screen_name": null,
+    "user": {
+      "id": 20709129,
+      "id_str": "20709129",
+      "name": "wastemobile",
+      "screen_name": "wastemobile",
+      "protected": false,
+      "verified": false,
+      "profile_image_url_https": "https://pbs.twimg.com/profile_images/1654041285/waste_normal.jpg"
+    },
+    "geo": null,
+    "coordinates": null,
+    "place": null,
+    "contributors": null,
+    "is_quote_status": false,
+    "retweet_count": 0,
+    "favorite_count": 0,
+    "favorited": false,
+    "retweeted": false,
+    "possibly_sensitive": false,
+    "lang": "zh"
+  },
+  {
     "created_at": "Mon Aug 01 01:15:56 +0000 2022",
     "id": 1553912380429848577,
     "id_str": "1553912380429848577",
